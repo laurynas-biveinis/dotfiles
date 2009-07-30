8e46b3b0ba2b87f842307530c6be64b91418542a
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2009-07-30 09:52:32 +0000

    Refresh dired buffers automatically

--- default-modes.el
+++ default-modes.el
@@ -69,3 +69,22 @@
 (put 'dired-find-alternate-file 'disabled nil)
 
 (setq wdired-allow-to-change-permissions t)
+
+; Refresh dired buffers on redisplay or buffer change
+; From http://nflath.com/2009/07/dired/
+(defadvice switch-to-buffer-other-window
+  (after auto-refresh-dired (buffer &optional norecord) activate)
+  (if (equal major-mode 'dired-mode)
+      (revert-buffer)))
+(defadvice switch-to-buffer
+  (after auto-refresh-dired (buffer &optional norecord) activate)
+  (if (equal major-mode 'dired-mode)
+      (revert-buffer)))
+(defadvice display-buffer
+  (after auto-refresh-dired (buffer &optional not-this-window frame)  activate)
+  (if (equal major-mode 'dired-mode)
+      (revert-buffer)))
+(defadvice other-window
+  (after auto-refresh-dired (arg &optional all-frame) activate)
+  (if (equal major-mode 'dired-mode)
+      (revert-buffer)))
