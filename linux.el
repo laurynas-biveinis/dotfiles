2ad34db6a42b970e427534f5e17b9c10f28a8fac
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2009-10-30 11:11:44 +0000

    Change default Linux font for Emacs 23

--- linux.el
+++ linux.el
@@ -4,10 +4,8 @@
   "Things that must be set on Linux before main setup"
 
   ; Default font
-  (if (>= emacs-major-version 23)
-      (add-to-list 'default-frame-alist '(font . "Bitstream Vera Sans Mono-8"))
-    (add-to-list 'default-frame-alist
-		 '(font . "-Misc-Fixed-Medium-R-Normal--14-130-75-75-C-70-ISO8859-1"))))
+  (add-to-list 'default-frame-alist
+	       '(font . "-Misc-Fixed-Medium-R-Normal--14-130-75-75-C-70-ISO8859-1")))
 
 (defun system-specific-setup()
   "Setup specifics for Linux"
