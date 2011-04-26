ba0b1aa21b478f1d7414708c27af36a463d814b4
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2011-04-26 05:20:56 +0000

    SQL mode tweak

--- default-modes.el
+++ default-modes.el
@@ -243,3 +243,14 @@
 ; sql-mode
 (add-to-list 'auto-mode-alist '("\\.test\\'" . sql-mode)) ; MySQL test files
 
+; Thanks to Alexey Kopytov
+(add-hook 'sql-mode-hook 'my-sql-mode-hook)
+(defun my-sql-mode-hook ()
+  (define-key sql-mode-map (kbd "RET") 'newline-and-indent)
+
+  ;; Make # start a new line comment in SQL. This is a MySQL-specific
+  ;; syntax.
+
+  (modify-syntax-entry ?# "< b" sql-mode-syntax-table)
+  (set-syntax-table sql-mode-syntax-table)
+)
