3efc5a46616f13412928ce7532cff12f82b6e4e9
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2011-09-25 20:44:09 +0300

    Remove obsolete file

--- runme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cp dot-emacs.el ../.emacs
-emacs -batch -f batch-byte-compile ../.emacs
-#(cd lib/cedet && emacs -Q -l cedet-build.el -f cedet-build)
-#find . -path '*/.svn' -prune -o -type d -exec emacs -batch -f batch-byte-compile '{}'/*.el ';'
-echo Remainder of byte compilation is up to you
