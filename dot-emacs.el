2f5e771bea727e8b3f5452a204d0774b5b1f677b
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2012-09-30 18:52:59 +0300

    Remove obsolete tweaks

--- dot-emacs.el
+++ dot-emacs.el
@@ -99,11 +99,5 @@
 (system-specific-setup)
 (load "projects")
 (load "load-desktop")
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(org-todo ((t (:foreground "red" :weight bold)))))
 
 (server-start)
