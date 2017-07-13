29266c3fb17f00c7f8f5672dbdd78d28551252e6
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2017-07-13 17:40:40 +0300

    Update

--- dot-emacs.el
+++ dot-emacs.el
@@ -92,6 +92,8 @@
        (load
         (expand-file-name (concat elpa-dir "package.el")))))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (load "addon-modes")
@@ -101,3 +103,15 @@
 (load "load-desktop")
 
 (server-start)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit org-plus-contrib org auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
