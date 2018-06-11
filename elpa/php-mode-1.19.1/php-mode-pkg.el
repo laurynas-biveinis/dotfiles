4d6773d6f4832c6e3235d8416f83a2b153bbfbd7
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2018-06-11 10:52:39 +0300

    ELPA update

---
+++ elpa/php-mode-1.19.1/php-mode-pkg.el
@@ -0,0 +1,9 @@
+(define-package "php-mode" "1.19.1" "Major mode for editing PHP code"
+  '((emacs "24")
+    (cl-lib "0.5"))
+  :keywords
+  '("languages" "php")
+  :url "https://github.com/ejmr/php-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
