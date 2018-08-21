eb6dac6c9bac8ba3e19a925a66f96a88f8f90b91
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2018-08-21 08:53:40 +0300

    ELPA update; install cmake-mode, cmake-font-lock

---
+++ elpa/cmake-mode-3.12.1/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "3.12.1" "major-mode for editing CMake sources" 'nil :commit "c4ab098097f4fe1bc85ee29c6de390dff435f63f")
--- elpa/org-20180820/org-pkg.el
+++ elpa/org-20180820/org-pkg.el
@@ -1,5 +1,5 @@
 (define-package "org"
-  "20180813" "Outline-based notes management and organizer" ( ))
+  "20180820" "Outline-based notes management and organizer" ( ))
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
