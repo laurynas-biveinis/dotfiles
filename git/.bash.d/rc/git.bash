5ac5ba55cd7df6ade312ae39ba1c3eeba529849c
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2018-10-25 15:06:32 +0300

    Include brew bash git completion only if it exists

--- git/.bash.d/rc/git.bash
+++ git/.bash.d/rc/git.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source /usr/local/etc/bash_completion.d/git-completion.bash
+if [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
+    source /usr/local/etc/bash_completion.d/git-completion.bash
+fi
 
 alias cf_on="git config --local include.path ../.gitconfig"
 alias cf_off="git config --local --unset include.path"
