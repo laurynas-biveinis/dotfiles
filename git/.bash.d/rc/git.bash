978c5be34ade0e027515003ad3576a6a32d42674
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2018-10-28 13:30:31 +0200

    Don't include git-only bash completion, it is already handled by global bash completion

--- git/.bash.d/rc/git.bash
+++ git/.bash.d/rc/git.bash
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-if [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
-    source /usr/local/etc/bash_completion.d/git-completion.bash
-fi
-
 alias cf_on="git config --local include.path ../.gitconfig"
 alias cf_off="git config --local --unset include.path"
