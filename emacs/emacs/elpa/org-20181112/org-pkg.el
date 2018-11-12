430e0b689e9f4b39a436c22a4dec5246e8eefaa4
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2018-11-12 08:53:24 +0200

    Update Emacs packages

--- emacs/emacs/elpa/org-20181112/org-pkg.el
+++ emacs/emacs/elpa/org-20181112/org-pkg.el
@@ -1,5 +1,5 @@
 (define-package "org"
-  "20181105" "Outline-based notes management and organizer" ( ))
+  "20181112" "Outline-based notes management and organizer" ( ))
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
