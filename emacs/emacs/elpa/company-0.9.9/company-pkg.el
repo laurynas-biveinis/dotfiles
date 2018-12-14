3d03dde3b11a20160f4c3f666f878a746a8fccb0
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2018-12-14 08:16:31 +0200

    Emacs package update

--- emacs/emacs/elpa/company-0.9.9/company-pkg.el
+++ emacs/emacs/elpa/company-0.9.9/company-pkg.el
@@ -1,4 +1,4 @@
-(define-package "company" "0.9.8" "Modular text completion framework"
+(define-package "company" "0.9.9" "Modular text completion framework"
   '((emacs "24.3"))
   :keywords
   '("abbrev" "convenience" "matching")
