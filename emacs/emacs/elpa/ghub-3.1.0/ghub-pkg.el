4ee5629ce82d117ac05cfdca5f3f1b0c07bd35f0
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2018-12-21 07:19:56 +0200

    Emacs package update

--- emacs/emacs/elpa/ghub-3.1.0/ghub-pkg.el
+++ emacs/emacs/elpa/ghub-3.1.0/ghub-pkg.el
@@ -1,4 +1,4 @@
-(define-package "ghub" "3.0.0" "Minuscule client libraries for Git forge APIs."
+(define-package "ghub" "3.1.0" "Minuscule client libraries for Git forge APIs."
   '((emacs "25.1")
     (dash "2.14.1")
     (graphql "0.1.1")
