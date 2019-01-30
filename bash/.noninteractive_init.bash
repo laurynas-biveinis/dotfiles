c29d91e30ed479895fc48b73b48c51076c586574
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-01-30 11:23:50 +0200

    Add support for linuxbrew

--- bash/.noninteractive_init.bash
+++ bash/.noninteractive_init.bash
@@ -9,6 +9,10 @@ if [ -d /usr/lib/ccache ]; then
     export PATH=/usr/lib/ccache:$PATH
 fi
 
+if [ -d /home/linuxbrew/.linuxbrew ]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
 export PATH=~/usr/bin:/usr/local/bin:$PATH
 
 for script in ~/.bash.d/noninteractive_init/*; do
