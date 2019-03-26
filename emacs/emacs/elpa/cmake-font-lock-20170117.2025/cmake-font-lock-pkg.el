3b2c7c74c8ee41a0834a40a676fbe5ece2f36b9f
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-03-26 08:00:37 +0200

    Emacs package update

--- emacs/emacs/elpa/cmake-font-lock-20170117.2025/cmake-font-lock-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cmake-font-lock" "20170117.2025" "Advanced, type aware, highlight support for CMake" '((cmake-mode "0.0")) :commit "8be491b4b13338078e524e2fe6213c93e18a101e" :keywords '("faces" "languages") :authors '(("Anders Lindgren")) :maintainer '("Anders Lindgren") :url "https://github.com/Lindydancer/cmake-font-lock")
