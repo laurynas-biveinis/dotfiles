f17af62ba46a087f6da9d4f8a13dacfd709f6e4d
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-04-29 08:23:31 +0300

    Emacs: install yaml mode

---
+++ emacs/emacs/elpa/yaml-mode-0.0.13/yaml-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yaml-mode" "0.0.13" "Major mode for editing YAML files" '((emacs "24.1")) :commit "2ace378bef2047a980fba0e42e3e6b5d990f2c66" :keywords '("data" "yaml") :authors '(("Yoshiki Kurihara" . "clouder@gmail.com") ("Marshall T. Vandegrift" . "llasram@gmail.com")) :maintainer '("Vasilij Schneidermann" . "v.schneidermann@gmail.com"))
