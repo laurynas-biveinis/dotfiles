52315da1596f6e4c1bd83520c8628a850139e621
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-06-12 09:24:27 +0300

    Emacs package update

--- emacs/emacs/elpa/org-plus-contrib-20190611/org-version.el
+++ emacs/emacs/elpa/org-plus-contrib-20190611/org-version.el
@@ -5,13 +5,13 @@
 (defun org-release ()
   "The release version of Org.
 Inserted by installing Org mode or when a release is made."
-   (let ((org-release "9.2.3"))
+   (let ((org-release "9.2.4"))
      org-release))
 ;;;###autoload
 (defun org-git-version ()
   "The Git version of Org mode.
 Inserted by installing Org or when a release is made."
-   (let ((org-git-version "9.2.3-23-g967801-elpaplus"))
+   (let ((org-git-version "9.2.4-elpaplus"))
      org-git-version))
 ;;;###autoload
 (defvar org-odt-data-dir "/usr/share/emacs/etc/org"
