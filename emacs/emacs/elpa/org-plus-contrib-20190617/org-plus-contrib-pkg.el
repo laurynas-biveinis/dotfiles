bac9a060fff1b91d9f26075c0f9f2aae088a4df8
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-06-17 09:13:33 +0300

    Emacs package update

--- emacs/emacs/elpa/org-plus-contrib-20190617/org-plus-contrib-pkg.el
+++ emacs/emacs/elpa/org-plus-contrib-20190617/org-plus-contrib-pkg.el
@@ -1,5 +1,5 @@
 (define-package "org-plus-contrib"
-  "20190611" "Outline-based notes management and organizer" ( ))
+  "20190617" "Outline-based notes management and organizer" ( ))
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
