49d21fcce43d46e0ce9d345edc9db9746757f7a4
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-09-18 12:20:51 +0100

    Emacs: cleanup darwin.el
    Add a missing require, declare an external function.

--- emacs/emacs/darwin.el
+++ emacs/emacs/darwin.el
@@ -4,8 +4,9 @@
 
 ;;; Code:
 
-;; Variables defined elsewhere we'll be using
+;; Variables and functions defined elsewhere we'll be using
 (defvar exec-path-from-shell-check-startup-files)
+(declare-function exec-path-from-shell-initialize "exec-path-from-shell" ())
 
 (defconst my-frame-font
   "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1"
@@ -20,6 +21,7 @@
 (defun system-specific-setup()
   "Setup specifics for Darwin."
   (setq ns-right-alternate-modifier 'none)
+  (require 'exec-path-from-shell)
   (setq exec-path-from-shell-check-startup-files nil)
   (exec-path-from-shell-initialize))
 
