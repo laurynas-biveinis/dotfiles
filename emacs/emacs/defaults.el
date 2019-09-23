50389f31e4ba32e9839e796915adcd07906654e5
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-09-23 11:46:14 +0300

    Emacs: fold misc.el into defaults.el

--- emacs/emacs/defaults.el
+++ emacs/emacs/defaults.el
@@ -194,6 +194,25 @@
 
 (add-hook 'find-file-hook #'treat-new-files-as-modified)
 
+(defun follow-cygwin-symlinks ()
+  "Follow Cygwin symlinks.
+Handles old-style (text file) and new-style (.lnk file) symlinks.
+\(Non-Cygwin-symlink .lnk files, such as desktop shortcuts, are still
+loaded as such.)"
+  (save-excursion
+    (goto-char 0)
+    (if (looking-at
+         "L\x000\x000\x000\x001\x014\x002\x000\x000\x000\x000\x000\x0C0\x000\x000\x000\x000\x000\x000\x046\x00C")
+        (progn
+          (re-search-forward
+           "\x000\\([-A-Za-z0-9_\\.\\\\\\$%@(){}~!#^'`][-A-Za-z0-9_\\.\\\\\\$%@(){}~!#^'`]+\\)")
+          (find-alternate-file (match-string 1)))
+      (when (looking-at "!<symlink>")
+        (re-search-forward "!<symlink>\\(.*\\)\0")
+        (find-alternate-file (match-string 1))))))
+
+(add-hook 'find-file-hook #'follow-cygwin-symlinks)
+
 ;; Mark executable files as executable on save
 (add-hook 'after-save-hook
           #'executable-make-buffer-file-executable-if-script-p)
