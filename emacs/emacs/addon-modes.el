198a6624c0c9957849450df78ac9894fd371258a
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-09-27 09:51:56 +0300

    Emacs: disable auto-indent-mode in erc-mode

--- emacs/emacs/addon-modes.el
+++ emacs/emacs/addon-modes.el
@@ -55,6 +55,7 @@
 (add-to-list 'auto-indent-disabled-modes-list #'Info-mode)
 (add-to-list 'auto-indent-disabled-modes-list #'magit-status-mode)
 (add-to-list 'auto-indent-disabled-modes-list #'org-agenda-mode)
+(add-to-list 'auto-indent-disabled-modes-list #'erc-mode)
 (auto-indent-global-mode)
 ;; Leave tabs/spaces alone on paste. TODO(laurynas): we would like to DTRT
 ;; instead, not sure how
