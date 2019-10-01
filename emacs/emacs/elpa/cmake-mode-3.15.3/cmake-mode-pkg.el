4289bcdc36540ccb12117ca6327848f6c994562a
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-10-01 07:39:05 +0300

    Emacs package update

--- emacs/emacs/elpa/cmake-mode-3.15.3/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "3.15.3" "major-mode for editing CMake sources" 'nil :commit "26a0e200e5f4abe8268235c9fdb23a2612a1b3b1")
---
+++ emacs/emacs/elpa/cmake-mode-3.15.4/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "3.15.4" "major-mode for editing CMake sources" 'nil :commit "6fb747a01c27a2226fe4cbf77bc19e6d21286f81")
