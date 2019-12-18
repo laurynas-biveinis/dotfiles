4d7f0939b11ff9cf3c6457e7ef52f3759b664a17
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2019-12-18 16:47:59 +0200

    Emacs: simplify ssh-config-mode setup

--- emacs/emacs/setup.el
+++ emacs/emacs/setup.el
@@ -1054,12 +1054,7 @@ BUFFER, TARGET, NICK, SERVER, and PORT are ERC-provided."
 
 (add-hook 'prog-mode-hook #'lsp-deferred)
 
-;;; SSH config
-(autoload 'ssh-config-mode "ssh-config-mode" t)
-(add-to-list 'auto-mode-alist '("/\\.ssh/config\\'"     . ssh-config-mode))
-(add-to-list 'auto-mode-alist '("/sshd?_config\\'"      . ssh-config-mode))
-(add-to-list 'auto-mode-alist '("/known_hosts\\'"       . ssh-known-hosts-mode))
-(add-to-list 'auto-mode-alist '("/authorized_keys2?\\'" . ssh-authorized-keys-mode))
+;;; SSH config mode
 (add-hook 'ssh-config-mode-hook #'turn-on-font-lock)
 (add-hook 'ssh-config-mode-hook #'dotfiles--enable-show-trailing-ws)
 (add-hook 'ssh-config-mode-hook #'turn-on-auto-fill)
