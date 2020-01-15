70640dfab305c3058475b9d675663ccede66cdca
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-01-15 08:13:35 +0200

    Emacs package update

--- emacs/emacs/elpa/popup-0.5.3/popup-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "popup" "0.5.3" "Visual Popup User Interface" '((cl-lib "0.3")) :commit "46632ab9652dacad56fd961cd6def25a015170ae" :keywords '("lisp") :authors '(("Tomohiro Matsuyama" . "m2ym.pub@gmail.com")) :maintainer '("Tomohiro Matsuyama" . "m2ym.pub@gmail.com"))
