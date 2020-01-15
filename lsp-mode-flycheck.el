a6d72f3629416149aae9a06b7487e3d0daa833fc
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-01-15 16:14:38 +0200

    More tramp/flycheck debugging

--- lsp-mode-flycheck.el
+++ lsp-mode-flycheck.el
@@ -1,10 +1,28 @@
-
-(add-to-list package-archives '("melpa-stable" .
-                                "https://stable.melpa.org/packages/")))
+(add-to-list 'package-archives '("melpa-stable" .
+                                "https://stable.melpa.org/packages/"))
 (package-refresh-contents)
 (package-install 'flycheck)
 
+(setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-gcc c/c++-cppcheck))
+
+(global-flycheck-mode)
+
+(setq lsp-enable-snippet nil)
+(setq lsp-eldoc-render-all t)
+(setq lsp-before-save-edits nil)
+(setq lsp-restart 'auto-restart)
+
 (setq lsp-prefer-flymake nil)
+
+(require 'lsp-ui)
+(setq lsp-ui-sideline-ignore-duplicate t)
+(setq lsp-ui-sideline-show-symbol nil)
+
+(require 'lsp-ui-doc)
+(setq lsp-ui-doc-header t)
+(setq lsp-ui-doc-include-signature t)
+(setq lsp-ui-doc-position 'top)
+
 
 (defconst lsp-clients-clangd-tramp-executable "clangd")
 (defun lsp-clients--clangd-tramp-command ()
@@ -18,3 +36,16 @@
                   :priority -1
                   :server-id 'clangd-tramp
                   :remote? t))
+
+(add-hook 'prog-mode-hook #'lsp-deferred)
+
+(setq enable-remote-dir-locals t)
+
+(require 'tramp)
+;; The default level 3 is too noisy with showing each file shipped
+(setq tramp-verbose 4)
+;; Not sure I care about scp overhead in 2020. Also, maybe this will help with
+;; duplicated sshx/scpx paths in lsp-mode cache.
+(setq tramp-copy-size-limit nil)
+(setq tramp-default-method "scpx")
+(setq tramp-completion-reread-directory-timeout t)
