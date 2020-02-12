08e4f2a43d805c77b57f4bdd96eb5513c8f41f4a
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-02-12 08:06:13 +0200

    Emacs package update

--- emacs/emacs/elpa/lsp-mode-20200211.1904/lsp-mode-pkg.el
+++ emacs/emacs/elpa/lsp-mode-20200211.1904/lsp-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-mode" "20200210.1849" "LSP mode"
+(define-package "lsp-mode" "20200211.1904" "LSP mode"
   '((emacs "25.1")
     (dash "2.14.1")
     (dash-functional "2.14.1")
--- emacs/emacs/elpa/lsp-ui-20200211.804/lsp-ui-pkg.el
+++ emacs/emacs/elpa/lsp-ui-20200211.804/lsp-ui-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-ui" "20200210.1633" "UI modules for lsp-mode"
+(define-package "lsp-ui" "20200211.804" "UI modules for lsp-mode"
   '((emacs "25.1")
     (dash "2.14")
     (dash-functional "1.2.0")
