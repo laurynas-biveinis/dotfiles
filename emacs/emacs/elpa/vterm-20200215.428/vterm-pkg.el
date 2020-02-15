b81f494ff781fcc8a500328a72465ad4d6335028
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-02-15 11:05:01 +0200

    Emacs package update

--- emacs/emacs/elpa/vterm-20200215.428/vterm-pkg.el
+++ emacs/emacs/elpa/vterm-20200215.428/vterm-pkg.el
@@ -1,4 +1,4 @@
-(define-package "vterm" "20200210.704" "This package implements a terminal via libvterm"
+(define-package "vterm" "20200215.428" "This package implements a terminal via libvterm"
   '((emacs "25.1"))
   :keywords
   '("terminals")
