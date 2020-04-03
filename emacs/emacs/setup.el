1a8152256e08b16e4bd46c7132a19c994bb26634
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-04-03 13:24:47 +0300

    Emacs: try showing all LSP code actions

--- emacs/emacs/setup.el
+++ emacs/emacs/setup.el
@@ -1154,6 +1154,7 @@ BUFFER, TARGET, NICK, SERVER, and PORT are ERC-provided."
 (setq lsp-ui-sideline-show-symbol nil)
 (setq lsp-ui-peek-fontify 'always)
 (setq lsp-ui-peek-peek-height 30)
+(setq lsp-ui-sideline-actions-kind-regex ".*")
 
 (define-key lsp-ui-mode-map [remap xref-find-definitions]
   #'lsp-ui-peek-find-definitions)
