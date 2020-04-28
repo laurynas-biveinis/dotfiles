6cb42811307445fb1352174fecc1c1ebd3f666fa
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-04-28 06:59:14 +0300

    Emacs package update

--- emacs/emacs/elpa/lsp-mode-20200427.1612/lsp-mode.el
+++ emacs/emacs/elpa/lsp-mode-20200427.1612/lsp-mode.el
@@ -176,7 +176,7 @@ the buffer when it becomes large."
  If `lsp-semantic-highlighting' is set to `:immediate', semantic
  highlighting information received from the language server is
  applied immediately, and in full. If `lsp-semantic-highlighting'
- is set to `:deferred', semantic higlighting will be performed
+ is set to `:deferred', semantic highlighting will be performed
  after an idle timeout, and only within a limit region
  around `(point)' (see
  `lsp-semantic-highlighting-context-lines'). Compared to
@@ -1582,7 +1582,7 @@ WORKSPACE is the workspace that contains the progress token."
   :package-version '(lsp-mode . "6.3"))
 
 (defun lsp--diagnostics-modeline-statistics ()
-  "Caculate diagnostics statistics based on `lsp-diagnostics-modeline-scope'"
+  "Calculate diagnostics statistics based on `lsp-diagnostics-modeline-scope'"
   (let ((diagnostics (cond
                       ((equal :file lsp-diagnostics-modeline-scope)
                        (lsp--get-buffer-diagnostics))
@@ -2911,7 +2911,7 @@ executed is still alive. `current' the callback will be executed
 only if the original buffer is still selected. `tick' - the
 callback will be executed only if the buffer was not modified.
 `unchanged' - the callback will be executed only if the buffer
-hasnt changed and if the buffer is not modified.
+hasn't changed and if the buffer is not modified.
 
 ERROR-CALLBACK will be called in case the request has failed.
 If NO-MERGE is non-nil, don't merge the results but return alist workspace->result.
@@ -3928,7 +3928,7 @@ Added to `after-change-functions'."
   :type 'hook
   :group 'lsp-mode)
 
-(defcustom lsp-idle-delay 0.200
+(defcustom lsp-idle-delay 0.500
   "Debounce interval for `after-change-functions'. "
   :type 'number
   :group 'lsp-mode)
@@ -4660,6 +4660,64 @@ Stolen from `org-copy-visible'."
 
     (lsp--setup-markdown major-mode)))
 
+(defvar lsp--display-inline-image-alist
+  '((lsp--render-markdown
+     (:regexp
+      "!\\[.*?\\](data:image/[a-zA-Z]+;base64,\\([A-Za-z0-9+/\n]+?=*?\\)\\(|[^)]+\\)?)"
+      :sexp
+      (create-image
+       (base64-decode-string
+        (buffer-substring-no-properties (match-beginning 1) (match-end 1)))
+       nil t))))
+  "Replaced string regexp and function returning image.
+Each element should be:
+(MODE . (PROPERTY-LIST...))
+MODE (car) is function which is defined in `lsp-language-id-configuration'.
+Cdr should be list of PROPERTY-LIST.
+
+Each PROPERTY-LIST should have properties:
+:regexp  Regexp which determines what string is relpaced to image.
+         You should also get infomation of image, by parenthesis constructs.
+         By deafult, all matched string is replaced to image, but you can change
+         index of replaced string by keyword :replaced-index.
+
+:sexp    Return image when evaluated. You can use infomation of regexp
+         by using (match-beggining N), (match-end N) or (match-substring N).
+
+In addition, each can have property:
+:replaced-index  Determine index which is used to replace regexp to image.
+                 The value means first argument of `match-beggining' and `match-end'.
+                 If omitted, interpreted as index 0.
+")
+
+(defcustom lsp-display-inline-image t
+  "Showing inline image or not."
+  :group 'lsp-mode
+  :type 'boolean)
+
+(defun lsp--display-inline-image (mode)
+  "Add image property if available."
+  (let ((plist-list (cdr (assq mode lsp--display-inline-image-alist))))
+    (when (and (display-images-p) lsp-display-inline-image)
+      (cl-loop
+       for plist in plist-list
+       with regexp with replaced-index
+       do
+       (setq regexp (plist-get plist :regexp))
+       (setq replaced-index (or (plist-get plist :replaced-index) 0))
+
+       (font-lock-remove-keywords nil (list regexp replaced-index))
+       (let ((inhibit-read-only t))
+         (save-excursion
+           (goto-char (point-min))
+           (while (re-search-forward regexp nil t)
+             (set-text-properties
+              (match-beginning replaced-index) (match-end replaced-index)
+              nil)
+             (add-text-properties
+              (match-beginning replaced-index) (match-end replaced-index)
+              `(display ,(eval (plist-get plist :sexp)))))))))))
+
 (defun lsp--fontlock-with-mode (str mode)
   "Fontlock STR with MODE."
   (condition-case nil
@@ -4667,7 +4725,8 @@ Stolen from `org-copy-visible'."
         (insert str)
         (delay-mode-hooks (funcall mode))
         (cl-flet ((window-body-width () lsp-window-body-width))
-          (font-lock-ensure))
+          (font-lock-ensure)
+          (lsp--display-inline-image mode))
         (lsp--buffer-string-visible))
     (error str)))
 
