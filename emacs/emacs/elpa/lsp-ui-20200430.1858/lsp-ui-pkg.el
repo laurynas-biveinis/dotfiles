b246aa7407bba0e96bdb409aa3dd5a417c2d2c98
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-05-01 17:45:16 +0300

    Emacs package update

--- emacs/emacs/elpa/lsp-ui-20200430.1858/lsp-ui-pkg.el
+++ emacs/emacs/elpa/lsp-ui-20200430.1858/lsp-ui-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-ui" "20200402.839" "UI modules for lsp-mode"
+(define-package "lsp-ui" "20200430.1858" "UI modules for lsp-mode"
   '((emacs "25.1")
     (dash "2.14")
     (dash-functional "1.2.0")
