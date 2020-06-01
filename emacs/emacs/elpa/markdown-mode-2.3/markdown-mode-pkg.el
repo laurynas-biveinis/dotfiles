ca77aea2aa7f91b1adb3a10b182e9e3f0d11de65
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-06-01 07:13:56 +0300

    Emacs package update

--- emacs/emacs/elpa/markdown-mode-2.3/markdown-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "markdown-mode" "2.3" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "cde5c5d2bcce470c494b76e23cfe1364b6291c20" :keywords '("markdown" "github flavored markdown" "itex") :authors '(("Jason R. Blevins" . "jblevins@xbeta.org")) :maintainer '("Jason R. Blevins" . "jblevins@xbeta.org") :url "https://jblevins.org/projects/markdown-mode/")
---
+++ emacs/emacs/elpa/modern-cpp-font-lock-20200530.1010/modern-cpp-font-lock-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "modern-cpp-font-lock" "20200530.1010" "Font-locking for \"Modern C++\"" 'nil :commit "865955d0035382a17a7f03add0d00d0bd812b103" :keywords '("languages" "c++" "cpp" "font-lock") :authors '(("Ludwig PACIFICI" . "ludwig@lud.cc")) :maintainer '("Ludwig PACIFICI" . "ludwig@lud.cc") :url "https://github.com/ludwigpacifici/modern-cpp-font-lock")
