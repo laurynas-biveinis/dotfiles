d693eacb04aec5bfa16e686989ee557b6aba7eed
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-06-06 07:00:06 +0300

    Emacs package update

--- emacs/emacs/elpa/flycheck-20200605.1351/flycheck-pkg.el
+++ emacs/emacs/elpa/flycheck-20200605.1351/flycheck-pkg.el
@@ -1,10 +1,10 @@
-(define-package "flycheck" "20200527.1502" "On-the-fly syntax checking"
+(define-package "flycheck" "20200605.1351" "On-the-fly syntax checking"
   '((dash "2.12.1")
     (pkg-info "0.4")
     (let-alist "1.0.4")
     (seq "1.11")
     (emacs "24.3"))
-  :commit "ee0937ffe23ea859f5547f81756257c12e2c34d4" :keywords
+  :commit "e7b0b704c97e8903bce2ea739c3e28979d376969" :keywords
   '("convenience" "languages" "tools")
   :authors
   '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
