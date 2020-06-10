9da0ce9a285ccce57c1e2c243694c078c10f0824
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-06-10 10:23:23 +0300

    Emacs package update

---
+++ emacs/emacs/elpa/cmake-mode-3.18.0pre1/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "3.18.0pre1" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "596cfd26809793c7165b5e5047be133e0419c6e5")
--- emacs/emacs/elpa/popup-0.5.7/popup-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "popup" "0.5.7" "Visual Popup User Interface" '((emacs "26.1") (cl-lib "0.5")) :commit "c76d516629c6f063e68742b94b376fc9506536c8" :keywords '("lisp") :authors '(("Tomohiro Matsuyama" . "m2ym.pub@gmail.com")) :maintainer '("Tomohiro Matsuyama" . "m2ym.pub@gmail.com"))
