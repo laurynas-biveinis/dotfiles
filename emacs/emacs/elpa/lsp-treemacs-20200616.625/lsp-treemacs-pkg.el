760935f56f2e468286ddc4f4f3ed4a4a43979bda
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-06-17 07:38:15 +0300

    Emacs package update

--- emacs/emacs/elpa/lsp-treemacs-20200616.625/lsp-treemacs-pkg.el
+++ emacs/emacs/elpa/lsp-treemacs-20200616.625/lsp-treemacs-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-treemacs" "20200607.1943" "LSP treemacs"
+(define-package "lsp-treemacs" "20200616.625" "LSP treemacs"
   '((emacs "25.1")
     (dash "2.14.1")
     (dash-functional "2.14.1")
@@ -6,7 +6,7 @@
     (ht "2.0")
     (treemacs "2.5")
     (lsp-mode "6.0"))
-  :commit "90754e4189fc78be31f338383db3d8a2042fd8bc" :keywords
+  :commit "5d3f7b49a14b1dd7457c4dcf4233be83b6a4d637" :keywords
   '("languages")
   :authors
   '(("Ivan Yonchovski"))
