6587cacf93379428d647b2546d541e6152bb2c6f
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-06-23 06:45:10 +0300

    Emacs package update

---
+++ emacs/emacs/elpa/helm-core-3.6.3/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "3.6.3" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "6c7d4153a61d208ca5dd90e17070f4a8556d5177" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- emacs/emacs/elpa/helm-make-20200518.950/helm-make-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-make" "20200518.950" "Select a Makefile target with helm" 'nil :commit "cc310f194e2bb978c092ae57d3c611df9d8d22b3" :keywords '("makefile") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/helm-make")
