ed0cb900213a214db5d1a931bcaaaa6288bf645a
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-07-02 12:32:15 +0300

    Emacs package update

--- emacs/emacs/elpa/cmake-mode-3.18.0pre2/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "3.18.0pre2" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "e66fe75792a2fbe9f3ffe237c748008906ae7116")
---
+++ emacs/emacs/elpa/lsp-ui-7.0/lsp-ui-pkg.el
@@ -0,0 +1,16 @@
+(define-package "lsp-ui" "7.0" "UI modules for lsp-mode"
+  '((emacs "26.1")
+    (dash "2.14")
+    (dash-functional "1.2.0")
+    (lsp-mode "6.0")
+    (markdown-mode "2.3"))
+  :commit "840620f8f5a86440c1ce29e66910dcbcdcd7d815" :keywords
+  '("languages" "tools")
+  :authors
+  '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
+  :maintainer
+  '("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me")
+  :url "https://github.com/emacs-lsp/lsp-ui")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
