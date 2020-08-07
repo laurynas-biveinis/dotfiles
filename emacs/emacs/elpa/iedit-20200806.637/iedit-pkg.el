d8581129ac077706f24b20390105061277bd3dea
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-08-07 07:11:26 +0300

    Emacs package update

---
+++ emacs/emacs/elpa/iedit-20200806.637/iedit-pkg.el
@@ -0,0 +1,10 @@
+(define-package "iedit" "20200806.637" "Edit multiple regions in the same way simultaneously." 'nil :commit "8abf2c1f4f0ade64cbb06c8f47055f04ab83e8d6" :keywords
+  '("occurrence" "region" "simultaneous" "refactoring")
+  :authors
+  '(("Victor Ren" . "victorhge@gmail.com"))
+  :maintainer
+  '("Victor Ren" . "victorhge@gmail.com")
+  :url "https://github.com/victorhge/iedit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
