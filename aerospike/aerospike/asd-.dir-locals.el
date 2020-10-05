87defb0a05889aa845c833d8e6d273b3edf53ffb
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-10-05 16:57:10 +0300

    Aerospike / Emacs: remove -o from Projectile git ls-files command
    It is incompatible with --recurse-submodules

--- aerospike/aerospike/asd-.dir-locals.el
+++ aerospike/aerospike/asd-.dir-locals.el
@@ -1,4 +1,4 @@
 ((nil . ((compilation-read-command . nil)
          ;; TODO(laurynas): consider setting compile-command too so that M-x compile DTRT
-         (projectile-generic-command . "(cd ce && git ls-files -zco --exclude-standard --recurse-submodules | gsed -z 's/^/ce\\//g'); (cd ee && git ls-files -zco --exclude-standard --recurse-submodules | gsed -z 's/^/ee\\//g')")))
+         (projectile-generic-command . "(cd ce && git ls-files -zc --exclude-standard --recurse-submodules | gsed -z 's/^/ce\\//g'); (cd ee && git ls-files -zc --exclude-standard --recurse-submodules | gsed -z 's/^/ee\\//g')")))
  (c-mode . ((c-file-style . "aerospike"))))
