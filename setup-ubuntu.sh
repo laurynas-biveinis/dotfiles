81b041d3a67020e92fb64c7dcb0f273668745c6b
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-11-11 18:11:35 +0200

    Ubuntu: tweak installation for Ubuntu 20.04 on AWS EC2

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -2,18 +2,20 @@
 
 # Common
 
-sudo apt-get install acpi stow tldr git-doc diffutils-doc perl-doc make
-sudo apt-get install python-pip build-essential gdb manpages-dev binutils \
-    binutils-doc cpp g++ gcc libasan5 liblsan0 libtsan0 libubsan1 libc6-dev \
-    cpp-doc gcc-doc autoconf automake libtool flex bison libasan5-dbg \
-    liblsan0-dbg libtsan0-dbg libubsan1-dbg glibc-doc make-doc \
-    autoconf-archive gnu-standards autoconf-doc gettext bison-doc flex-doc \
-    libgcc1-dbg libgomp1-dbg libitm1-dbg libatomic1-dbg libmpx2-dbg \
-    libquadmath0-dbg gdb-doc gettext-doc libtool-doc m4-doc python-doc cmake \
-    cmake-doc diffstat unzip pinentry-doc zip software-properties-common \
-    colordiff valgrind linux-tools-generic libjemalloc2 python3-scipy \
-    python-numpy-doc python3-pytest python3-numpy-dbg python-scipy-doc \
-    unattended-upgrades screen colordiff fzf hexyl ripgrep fd-find
+sudo apt-get install stow tldr git-doc diffutils-doc perl-doc make
+# Not found on AWS EC2
+sudo apt-get install acpi python-pip libasan5-dbg liblsan0-dbg libtsan0-dbg \
+     libubsan1-dbg libgcc1-dbg libgomp1-dbg libitm1-dbg libatomic1-dbg \
+     libmpx2-dbg libquadmath0-dbg python-doc
+sudo apt-get install build-essential gdb manpages-dev binutils binutils-doc \
+     cpp g++ gcc libasan5 liblsan0 libtsan0 libubsan1 libc6-dev cpp-doc \
+     gcc-doc autoconf automake libtool flex bison glibc-doc make-doc \
+     autoconf-archive gnu-standards autoconf-doc gettext bison-doc flex-doc \
+     gdb-doc gettext-doc libtool-doc m4-doc cmake cmake-doc diffstat unzip \
+     pinentry-doc zip software-properties-common colordiff valgrind \
+     linux-tools-generic libjemalloc2 python3-scipy python-numpy-doc \
+     python3-pytest python3-numpy-dbg python-scipy-doc unattended-upgrades \
+     screen colordiff fzf hexyl ripgrep fd-find
 # Not named fd by default because fdclone (which I don't use) was first
 sudo ln -sf /usr/bin/fdfind /usr/local/bin/fd
 # So that I can have "gsed" as a GNU Sed on any platform
@@ -21,14 +23,6 @@ sudo ln -sf /bin/sed /bin/gsed
 # Home
 sudo apt-get install g++-8 gcc-8-doc libstdc++6-8-dbg libstdc++-8-doc \
     libboost-dev libboost-doc
-# Work
-sudo apt-get install pkg-config libev-dev libssl-dev libssl-doc libldap2-dev \
-    zlib1g-dev libreadline-dev readline-doc ncurses-doc pex python-pex-doc \
-    python-secretstorage-doc python-setuptools-doc openjdk-8-jre openjdk-8-jdk \
-    maven clang-tools-10 clang-10-doc llvm-10-doc bear libcurl4-openssl-dev \
-    libcurl4-doc docker docker-doc python3-pip apparmor-profiles libuv1-dev \
-    liblua5.1-dev
-
 sudo usermod -aG docker laurynas
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo add-apt-repository \
@@ -36,7 +30,14 @@ sudo add-apt-repository \
 sudo apt-get install clangd-10 clang-format-10
 sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-10 100
 sudo update-alternatives --install /usr/bin/clang-format clang-format \
-    /usr/bin/clang-format-10 100
+     /usr/bin/clang-format-10 100
+# Work
+sudo apt-get install pkg-config libev-dev libssl-dev libssl-doc libldap2-dev \
+    zlib1g-dev libreadline-dev readline-doc ncurses-doc pex python-pex-doc \
+    python-secretstorage-doc python-setuptools-doc openjdk-8-jre openjdk-8-jdk \
+    maven clang-tools-10 clang-10-doc llvm-10-doc bear libcurl4-openssl-dev \
+    libcurl4-doc docker docker-doc python3-pip apparmor-profiles libuv1-dev \
+    liblua5.1-dev
 # MySQL development specific
 sudo apt-get install ccache rapidjson-dev valgrind-dbg libboost-container-dev \
     libboost-doc clang clang-8-doc llvm-8-doc clang-format clang-tidy cppcheck \
@@ -64,4 +65,3 @@ sudo ln -s /usr/lib/debug/lib/x86_64-linux-gnu/* /usr/lib/debug/usr/lib/x86_64-l
 # Edit `/etc/sysctl.d/10-ptrace.conf` for `kernel.yama.ptrace_scope = 0`
 
 sudo sh -c "echo 0 > /proc/sys/kernel/yama/ptrace_scope"
-
