1e0bd16ea3f9a8668b474b77681eb6a3991b015a
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-11-13 05:46:39 +0200

    Emacs package update

---
+++ emacs/.emacs.d/elpa/vterm-20201112.1629/vterm-pkg.el
@@ -0,0 +1,12 @@
+(define-package "vterm" "20201112.1629" "Fully-featured terminal emulator"
+  '((emacs "25.1"))
+  :commit "9469bdf461dfe33057e049d611e12a8e533054de" :keywords
+  ("terminals")
+  :authors
+  (("Lukas Fürmetz" . "fuermetz@mailbox.org"))
+  :maintainer
+  ("Lukas Fürmetz" . "fuermetz@mailbox.org")
+  :url "https://github.com/akermu/emacs-libvterm")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- emacs/.emacs.d/package-quickstart.el
+++ emacs/.emacs.d/package-quickstart.el
@@ -666,10 +666,10 @@ See `wakatime-mode' for more information on Wakatime mode.
 
 
 )
-(let ((load-file-name "/Users/laurynas/.emacs.d/elpa/vterm-20201004.2057/vterm-autoloads.el"))
+(let ((load-file-name "/Users/laurynas/.emacs.d/elpa/vterm-20201112.1629/vterm-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/vterm-20201004.2057/vterm-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/vterm-20201112.1629/vterm-autoloads.el") (car load-path))))
 
 
 
