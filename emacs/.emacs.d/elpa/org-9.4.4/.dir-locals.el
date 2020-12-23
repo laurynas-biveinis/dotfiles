dedd8a9154df6f653a38e80ebbfb1ed4e7a0b375
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2020-12-23 08:20:49 +0200

    Emacs package update

---
+++ emacs/.emacs.d/elpa/org-9.4.4/.dir-locals.el
@@ -0,0 +1,20 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (indent-tabs-mode . t)
+  (tab-width . 8)
+  (fill-column . 70)
+  (sentence-end-double-space . t))
+ (org-mode
+  (indent-tabs-mode)
+  (org-adapt-indentation)
+  (org-edit-src-content-indentation . 0)
+  (org-footnote-auto-adjust . t)
+  (org-footnote-auto-label . t)
+  (org-footnote-define-inline . nil)
+  (org-footnote-section . "Footnotes")
+  (org-hide-emphasis-markers . nil)))
+
+
+
