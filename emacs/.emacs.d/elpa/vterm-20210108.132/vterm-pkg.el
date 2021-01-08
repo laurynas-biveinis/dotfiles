8e7a5fe9a69512e714e03683a259d7d1ef2367c2
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2021-01-08 06:57:31 +0200

    Emacs package update

---
+++ emacs/.emacs.d/elpa/vterm-20210108.132/vterm-pkg.el
@@ -0,0 +1,12 @@
+(define-package "vterm" "20210108.132" "Fully-featured terminal emulator"
+  '((emacs "25.1"))
+  :commit "b6592f94d76098d19f242c0d09f660f6a6461b76" :authors
+  '(("Lukas Fürmetz" . "fuermetz@mailbox.org"))
+  :maintainer
+  '("Lukas Fürmetz" . "fuermetz@mailbox.org")
+  :keywords
+  '("terminals")
+  :url "https://github.com/akermu/emacs-libvterm")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
