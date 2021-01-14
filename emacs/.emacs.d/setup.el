0f07781db212b9a33962189b26a5f9fd7ae2cb71
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2021-01-14 15:39:03 +0200

    Emacs: byte compilation fix

--- emacs/.emacs.d/setup.el
+++ emacs/.emacs.d/setup.el
@@ -666,7 +666,9 @@ respectively."
 ;; duplicated sshx/scpx paths in lsp-mode cache.
 (setq tramp-copy-size-limit nil)
 (setq tramp-default-method "scpx")
-(setq tramp-completion-reread-directory-timeout t)
+;; Remove `with-suppressed-warnings' once upgraded to 27.2
+(with-suppressed-warnings ((obsolete tramp-completion-reread-directory-timeout))
+  (setq tramp-completion-reread-directory-timeout t))
 
 ;; TRAMP "integration" with VC: I only use git on remote hosts, handled by
 ;; Magit, thus disable VC over TRAMP
