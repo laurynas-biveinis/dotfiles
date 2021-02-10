73bc4122542f21d903a344744c6a9d9fc2b89a03
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2021-02-10 06:20:45 +0200

    Emacs package update

--- emacs/.emacs.d/elpa/vterm-20210209.356/vterm-pkg.el
+++ emacs/.emacs.d/elpa/vterm-20210209.356/vterm-pkg.el
@@ -1,6 +1,6 @@
-(define-package "vterm" "20210207.357" "Fully-featured terminal emulator"
+(define-package "vterm" "20210209.356" "Fully-featured terminal emulator"
   '((emacs "25.1"))
-  :commit "7adecaa48c222f2567d503705547cf239e38fc4b" :authors
+  :commit "a3fadd28370aa43f03d4f7b197be8fa074f311f5" :authors
   '(("Lukas Fürmetz" . "fuermetz@mailbox.org"))
   :maintainer
   '("Lukas Fürmetz" . "fuermetz@mailbox.org")
