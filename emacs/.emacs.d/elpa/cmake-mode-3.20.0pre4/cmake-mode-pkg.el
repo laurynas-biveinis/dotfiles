2dadbcd5bcea602135b35a6d0ad15db0d55cef21
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2021-03-12 09:07:45 +0200

    Emacs package update

---
+++ emacs/.emacs.d/elpa/cmake-mode-3.20.0pre4/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/laurynas/.emacs.d/elpa/cmake-mode-3.20.0pre4/cmake-mode.el  -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "3.20.0pre4" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "c69d4b177fb3f6ebd22cddec919dbe1c30f43714")
--- emacs/.emacs.d/elpa/cmake-mode-3.20.0pre4/cmake-mode.el
+++ emacs/.emacs.d/elpa/cmake-mode-3.20.0pre4/cmake-mode.el
@@ -1,8 +1,8 @@
 ;;; cmake-mode.el --- major-mode for editing CMake sources
 
 ;; Package-Requires: ((emacs "24.1"))
-;; Package-Version: 3.20.0pre3
-;; Package-Commit: 12a139f2b6610e688be3d2bd317d46f0ce6ddf1f
+;; Package-Version: 3.20.0pre4
+;; Package-Commit: c69d4b177fb3f6ebd22cddec919dbe1c30f43714
 
 ; Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 ; file Copyright.txt or https://cmake.org/licensing for details.
