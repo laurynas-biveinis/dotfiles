ca4db2b3c702a8b1293df775b19dcf4a3e8c4ea3
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2021-03-15 06:53:31 +0200

    Emacs package update

--- emacs/.emacs.d/package-quickstart.el
+++ emacs/.emacs.d/package-quickstart.el
@@ -11987,10 +11987,10 @@ enable the mode if ARG is omitted or nil, and toggle it if ARG is
 
 
 )
-(let ((load-file-name "/Users/laurynas/.emacs.d/elpa/flycheck-20201228.2104/flycheck-autoloads.el"))
+(let ((load-file-name "/Users/laurynas/.emacs.d/elpa/flycheck-20210314.2015/flycheck-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/flycheck-20201228.2104/flycheck-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/flycheck-20210314.2015/flycheck-autoloads.el") (car load-path))))
 
 
 
