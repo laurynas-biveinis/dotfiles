d5c9e3bd58ce32d1afabe3d1582c51f397ceda03
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2021-04-03 11:58:15 +0300

    Emacs package update

--- emacs/.emacs.d/package-quickstart.el
+++ emacs/.emacs.d/package-quickstart.el
@@ -9904,10 +9904,10 @@ Fontify an `info' node." nil nil)
 
 
 )
-(let ((load-file-name "/Users/laurynas/.emacs.d/elpa/iedit-20210203.23/iedit-autoloads.el"))
+(let ((load-file-name "/Users/laurynas/.emacs.d/elpa/iedit-20210402.854/iedit-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/iedit-20210203.23/iedit-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/iedit-20210402.854/iedit-autoloads.el") (car load-path))))
 
 
 
