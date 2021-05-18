1cfec37f233e89561460ef83fdc6c785661bdc73
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2021-05-18 04:58:59 +0300

    Emacs package update

--- emacs/.emacs.d/package-quickstart.el
+++ emacs/.emacs.d/package-quickstart.el
@@ -1768,10 +1768,10 @@ Turn on `stripe-buffer-mode' and `hl-line-mode'." t nil)
 
 
 )
-(let ((load-file-name "/Users/laurynas/.emacs.d/elpa/ssh-config-mode-20210428.1752/ssh-config-mode-autoloads.el"))
+(let ((load-file-name "/Users/laurynas/.emacs.d/elpa/ssh-config-mode-20210517.1925/ssh-config-mode-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/ssh-config-mode-20210428.1752/ssh-config-mode-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/ssh-config-mode-20210517.1925/ssh-config-mode-autoloads.el") (car load-path))))
 
 
 
