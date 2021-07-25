4080099c456c08aebdb78fe5f1e016fe18536ee4
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2021-07-25 08:25:43 +0300

    Emacs package update

--- emacs/.emacs.d/elpa/ssh-config-mode-20210724.951/ssh-config-mode-pkg.el
+++ emacs/.emacs.d/elpa/ssh-config-mode-20210724.951/ssh-config-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "ssh-config-mode" "20210714.951" "Mode for fontification of ~/.ssh/config" 'nil :commit "1c8f8779f20479e55f9d3d1151f1f68c1dff56f2" :authors
+(define-package "ssh-config-mode" "20210724.951" "Mode for fontification of ~/.ssh/config" 'nil :commit "2642659aa4cb882d95d84f780e8f8bf5e3a9114b" :authors
   '(("Harley Gorrell" . "harley@panix.com"))
   :maintainer
   '("Harley Gorrell" . "harley@panix.com")
