8aef67b5e714427eae70bd6719a4eabb09317f87
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2021-09-09 16:00:07 +0300

    Emacs: do not try to start LSP for makefile-bsdmake-mode

--- emacs/.emacs.d/setup.el
+++ emacs/.emacs.d/setup.el
@@ -1476,7 +1476,7 @@ CANDIDATES is the list of candidates."
 
 (defun dotfiles--lsp-deferred-if-supported ()
   "Run `lsp-deferred' if it's a supported mode."
-  (unless (derived-mode-p 'emacs-lisp-mode)
+  (unless (derived-mode-p 'emacs-lisp-mode 'makefile-bsdmake-mode)
     (lsp-deferred)))
 
 (add-hook 'prog-mode-hook #'dotfiles--lsp-deferred-if-supported)
