d69bdaf1f0264185c2463c2a5ff72a8bd083d0e6
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-01-18 16:31:19 +0200

    Emacs/MySQL: use personal spelling dictionary

---
+++ mysql-work/vilniusdb/.dir-locals.el
@@ -0,0 +1,9 @@
+;; StackOverflow'ed from https://stackoverflow.com/a/11474657/80458
+;; An alternative would be to use `projectile-project-root', but the current
+;; method works without Projectile too.
+((nil . ((eval . (setq ispell-personal-dictionary
+                       (expand-file-name
+                        ".ispell.dict"
+                        (file-name-directory
+                         (let ((d (dir-locals-find-file "./")))
+                           (if (stringp d) d (car d))))))))))
