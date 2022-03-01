a7cc6149e66eb52cf9c5385ef970b1861b5e1cba
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-03-01 07:46:11 +0200

    Emacs package update

--- emacs/.emacs.d/elpa/tramp-2.5.2.1/tramp-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from tramp.el  -*- no-byte-compile: t -*-
-(define-package "tramp" "2.5.2.1" "Transparent Remote Access, Multiple Protocol" '((emacs "25.1")) :commit "974db74a36729d67dc758301e1cc9341396c79d9" :authors '(("Kai Großjohann" . "kai.grossjohann@gmx.net")) :maintainer '("Michael Albinus" . "michael.albinus@gmx.de") :keywords '("comm" "processes") :url "https://www.gnu.org/software/tramp/")
