aeea6e307156b20caa47ad3ea51e00c6b866c6ac
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-06-11 07:03:54 +0300

    Emacs package update

--- emacs/.emacs.d/elpa/yaml-0.3.5/yaml-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from yaml.el  -*- no-byte-compile: t -*-
-(define-package "yaml" "0.3.5" "YAML parser for Elisp" '((emacs "25.1")) :commit "adb3e52a214a5154267085639f95a3ffae1ec2d3" :authors '(("Zachary Romero" . "zkry@posteo.org")) :maintainer '("Zachary Romero" . "zkry@posteo.org") :keywords '("tools") :url "https://github.com/zkry/yaml.el")
