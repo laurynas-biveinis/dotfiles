4c9189d30b0222815f81f2572e930c5b01f10f2c
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-06-21 11:23:13 +0300

    Emacs package update
    At the same time cleanup old vterm version leftovers

---
+++ emacs/.emacs.d/elpa/wakatime-mode-20220621.616/wakatime-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from wakatime-mode.el  -*- no-byte-compile: t -*-
+(define-package "wakatime-mode" "20220621.616" "Automatic time tracking extension for WakaTime" 'nil :commit "a9c7ab37dbaeadb36b90bcd38b31e4447706b251" :authors '(("Gabor Torok" . "gabor@20y.hu")) :maintainer '("Alan Hamlett" . "alan@wakatime.com") :keywords '("calendar" "comm"))
