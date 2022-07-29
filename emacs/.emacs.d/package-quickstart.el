7d2cc03ae58e50e64fd87de542945cd7bff22066
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-07-29 05:10:27 +0300

    Emacs: remove org-jira

--- emacs/.emacs.d/package-quickstart.el
+++ emacs/.emacs.d/package-quickstart.el
@@ -2769,17 +2769,6 @@ the mode if ARG is omitted or nil, and toggle it if ARG is `toggle'.
 
 
 )
-(let ((load-true-file-name "/Users/laurynas/.emacs.d/elpa/request-0.3.2/request-autoloads.el")(load-file-name "/Users/laurynas/.emacs.d/elpa/request-0.3.2/request-autoloads.el"))
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/request-0.3.2/request-autoloads.el") (car load-path))))
-
-
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "request" '("request-")))
-
-
-)
 (let ((load-true-file-name "/Users/laurynas/.emacs.d/elpa/rainbow-delimiters-2.1.5/rainbow-delimiters-autoloads.el")(load-file-name "/Users/laurynas/.emacs.d/elpa/rainbow-delimiters-2.1.5/rainbow-delimiters-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
@@ -5868,197 +5857,6 @@ the reports is done using the TaskJuggler GUI.
 
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ox-texinfo" '("org-texinfo-")))
-
-
-
-
-)
-(let ((load-true-file-name "/Users/laurynas/.emacs.d/elpa/org-jira-4.4.0/org-jira-autoloads.el")(load-file-name "/Users/laurynas/.emacs.d/elpa/org-jira-4.4.0/org-jira-autoloads.el"))
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/org-jira-4.4.0/org-jira-autoloads.el") (car load-path))))
-
-
-
-(register-definition-prefixes "jiralib" '("jiralib-"))
-
-
-
-(autoload 'org-jira-mode "org-jira" "\
-Toggle org-jira mode.
-With no argument, the mode is toggled on/off.
-Non-nil argument turns mode on.
-Nil argument turns mode off.
-
-This is a minor mode.  If called interactively, toggle the
-`Org-jira mode' mode.  If the prefix argument is positive, enable
-the mode, and if it is zero or negative, disable the mode.
-
-If called from Lisp, toggle the mode if ARG is `toggle'.  Enable
-the mode if ARG is nil, omitted, or is a positive number.
-Disable the mode if ARG is a negative number.
-
-To check whether the minor mode is enabled in the current buffer,
-evaluate `org-jira-mode'.
-
-The mode's hook is called both when the mode is enabled and when
-it is disabled.
-
-Commands:
-\\{org-jira-entry-mode-map}
-
-Entry to this mode calls the value of `org-jira-mode-hook'.
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'org-jira-get-projects "org-jira" "\
-Get list of projects." t nil)
-
-(autoload 'org-jira-get-issue-list "org-jira" "\
-Get list of issues, using jql (jira query language), invoke CALLBACK after.
-
-Default is unresolved issues assigned to current login user; with
-a prefix argument you are given the chance to enter your own
-jql.
-
-\(fn &optional CALLBACK)" nil nil)
-
-(autoload 'org-jira-get-summary "org-jira" "\
-Get issue summary from point and place next to issue id from jira" t nil)
-
-(autoload 'org-jira-get-summary-url "org-jira" "\
-Get issue summary from point and place next to issue id from jira, and make issue id a link" t nil)
-
-(autoload 'org-jira-get-issues-headonly "org-jira" "\
-Get list of ISSUES, head only.
-
-The default behavior is to return issues assigned to you and unresolved.
-
-With a prefix argument, allow you to customize the jql.  See
-`org-jira-get-issue-list'.
-
-\(fn ISSUES)" t nil)
-
-(autoload 'org-jira-get-issue "org-jira" "\
-Get a JIRA issue, allowing you to enter the issue-id first.
-
-\(fn ID)" t nil)
-
-(autoload 'org-jira-get-issues-by-fixversion "org-jira" "\
-Get list of issues by FIXVERSION.
-
-\(fn FIXVERSION)" t nil)
-
-(autoload 'org-jira-get-issue-project "org-jira" "\
-
-
-\(fn ISSUE)" nil nil)
-
-(autoload 'org-jira-get-issues "org-jira" "\
-Get list of ISSUES into an org buffer.
-
-Default is get unfinished issues assigned to you, but you can
-customize jql with a prefix argument.
-See`org-jira-get-issue-list'
-
-\(fn ISSUES)" t nil)
-
-(autoload 'org-jira-get-issues-from-custom-jql "org-jira" "\
-Get JQL-LIST list of issues from a custom JQL and PROJ-KEY.
-
-The PROJ-KEY will act as the file name, while the JQL will be any
-valid JQL to populate a file to store PROJ-KEY results in.
-
-Please note that this is *not* concurrent or race condition
-proof.  If you try to run multiple calls to this function, it
-will mangle things badly, as they rely on globals DEFAULT-JQL and
-ORG-JIRA-PROJ-KEY-OVERRIDE being set before and after running.
-
-\(fn &optional JQL-LIST)" t nil)
-
-(autoload 'org-jira-update-comment "org-jira" "\
-Update a comment for the current issue." t nil)
-
-(autoload 'org-jira-update-worklogs-from-org-clocks "org-jira" "\
-Update or add a worklog based on the org clocks." t nil)
-
-(autoload 'org-jira-copy-current-issue-key "org-jira" "\
-Copy the current issue's key into clipboard." t nil)
-
-(autoload 'org-jira-unassign-issue "org-jira" "\
-Update an issue to be unassigned." t nil)
-
-(autoload 'org-jira-set-issue-reporter "org-jira" "\
-Update an issue's reporter interactively." t nil)
-
-(autoload 'org-jira-assign-issue "org-jira" "\
-Update an issue with interactive re-assignment." t nil)
-
-(autoload 'org-jira-update-issue "org-jira" "\
-Update an issue." t nil)
-
-(autoload 'org-jira-todo-to-jira "org-jira" "\
-Convert an ordinary todo item to a jira ticket." t nil)
-
-(autoload 'org-jira-get-subtasks "org-jira" "\
-Get subtasks for the current issue." t nil)
-
-(autoload 'org-jira-create-issue "org-jira" "\
-Create an issue in PROJECT, of type TYPE, with given SUMMARY and DESCRIPTION.
-
-\(fn PROJECT TYPE SUMMARY DESCRIPTION)" t nil)
-
-(autoload 'org-jira-create-subtask "org-jira" "\
-Create a subtask issue for PROJECT, of TYPE, with SUMMARY and DESCRIPTION.
-
-\(fn PROJECT TYPE SUMMARY DESCRIPTION)" t nil)
-
-(autoload 'org-jira-refresh-issue "org-jira" "\
-Refresh current issue from jira to org." t nil)
-
-(autoload 'org-jira-progress-issue "org-jira" "\
-Progress issue workflow." t nil)
-
-(autoload 'org-jira-progress-issue-next "org-jira" "\
-Progress issue workflow." t nil)
-
-(autoload 'org-jira-browse-issue "org-jira" "\
-Open the current issue in external browser." t nil)
-
-(autoload 'org-jira-download-attachment "org-jira" "\
-Download the attachment under cursor." t nil)
-
-(autoload 'org-jira-get-issues-from-filter "org-jira" "\
-Get issues from the server-side stored filter named FILTER.
-
-Provide this command in case some users are not able to use
-client side jql (maybe because of JIRA server version?).
-
-\(fn FILTER)" t nil)
-
-(autoload 'org-jira-get-issues-from-filter-headonly "org-jira" "\
-Get issues *head only* from saved filter named FILTER.
-See `org-jira-get-issues-from-filter'.
-
-\(fn FILTER)" t nil)
-
-(autoload 'org-jira-get-issues-by-board "org-jira" "\
-Get list of ISSUES from agile board." t nil)
-
-(autoload 'org-jira-get-issues-by-sprint "org-jira" "\
-Get list of ISSUES from sprint." t nil)
-
-(autoload 'org-jira-get-issues-by-board-headonly "org-jira" "\
-Get list of ISSUES from agile board, head only." t nil)
-
-(autoload 'org-jira-get-boards "org-jira" "\
-Get list of boards and their properies." t nil)
-
-(register-definition-prefixes "org-jira" '("ensure-on-" "org-jira-"))
-
-
-
-(register-definition-prefixes "org-jira-sdk" '("org-jira-sdk-"))
 
 
 
@@ -14270,7 +14068,7 @@ See `aggressive-indent-mode' for more information on Aggressive-Indent mode.
 )
 (setq package-activated-list
       (append
-       '(yasnippet yaml-mode xterm-color with-editor which-key wgrep wgrep-helm wakatime-mode vterm queue undo-tree tsc dash s avy ace-window pfuture lv hydra ht posframe cfrs treemacs tree-sitter tree-sitter-langs compat transient stripe-buffer ssh-config-mode ssh spinner solarized-theme rich-minority request rainbow-delimiters projectile popup epl pkg-info page-break-lines org-plus-contrib org-jira org org-contrib org-analyzer f markdown-mode company neuron-mode modern-cpp-font-lock magit-section git-commit magit lua-mode lsp-mode lsp-ui lsp-treemacs keyfreq info-colors iedit highlight-indent-guides async helm-core helm helm-projectile helm-org helm-make helm-lsp helm-icons helm-descbinds dash-docs helm-dash grab-mac-link google-c-style gitignore-mode gitconfig-mode gitattributes-mode git-gutter fringe-helper git-gutter-fringe gcmh flycheck flycheck-status-emoji flycheck-google-cpplint flycheck-color-mode-line exec-path-from-shell eldoc-cmake dispwatch deadgrep color-identifiers-mode cmake-mode cmake-font-lock cheat-sh calfw-org calfw-ical calfw bison-mode beginend beacon all-the-icons all-the-icons-dired aggressive-indent)
+       '(yasnippet yaml-mode xterm-color with-editor which-key wgrep wgrep-helm wakatime-mode vterm queue undo-tree tsc dash s avy ace-window pfuture lv hydra ht posframe cfrs treemacs tree-sitter tree-sitter-langs compat transient stripe-buffer ssh-config-mode ssh spinner solarized-theme rich-minority rainbow-delimiters projectile popup epl pkg-info page-break-lines org-plus-contrib org org-contrib org-analyzer f markdown-mode company neuron-mode modern-cpp-font-lock magit-section git-commit magit lua-mode lsp-mode lsp-ui lsp-treemacs keyfreq info-colors iedit highlight-indent-guides async helm-core helm helm-projectile helm-org helm-make helm-lsp helm-icons helm-descbinds dash-docs helm-dash grab-mac-link google-c-style gitignore-mode gitconfig-mode gitattributes-mode git-gutter fringe-helper git-gutter-fringe gcmh flycheck flycheck-status-emoji flycheck-google-cpplint flycheck-color-mode-line exec-path-from-shell eldoc-cmake dispwatch deadgrep color-identifiers-mode cmake-mode cmake-font-lock cheat-sh calfw-org calfw-ical calfw bison-mode beginend beacon all-the-icons all-the-icons-dired aggressive-indent)
        package-activated-list))
 (progn
   (require 'info)
