1216f75229936bfb0f32d28341f792a0044915c5
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-08-03 06:02:49 +0300

    macOS: install pip packages for Google Benchmark

--- setup-macos.sh
+++ setup-macos.sh
@@ -339,7 +339,7 @@ sudo ln -sf en_US.aff en_LT.aff
 sudo ln -sf en_US.dic en_LT.dic
 # sudo easy_install pip
 python3 -m ensurepip
-pip install asitop
+pip install asitop scipy pandas
 sudo gem install mdl
 brew install --cask rescuetime slack vlc disk-inventory-x google-chrome dash \
      telegram keycombiner michaelvillar-timer skype
