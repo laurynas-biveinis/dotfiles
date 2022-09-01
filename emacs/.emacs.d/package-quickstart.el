8e19981f4a755f5429c6fa5b363dadafe1117456
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-09-01 05:53:19 +0300

    Emacs package update

--- emacs/.emacs.d/package-quickstart.el
+++ emacs/.emacs.d/package-quickstart.el
@@ -528,10 +528,10 @@ See `wakatime-mode' for more information on Wakatime mode.
 
 
 )
-(let ((load-true-file-name "/Users/laurynas/.emacs.d/elpa/vterm-20220830.320/vterm-autoloads.el")(load-file-name "/Users/laurynas/.emacs.d/elpa/vterm-20220830.320/vterm-autoloads.el"))
+(let ((load-true-file-name "/Users/laurynas/.emacs.d/elpa/vterm-20220831.835/vterm-autoloads.el")(load-file-name "/Users/laurynas/.emacs.d/elpa/vterm-20220831.835/vterm-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/vterm-20220830.320/vterm-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/vterm-20220831.835/vterm-autoloads.el") (car load-path))))
 
 
 
