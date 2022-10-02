765bfa15ddabfd9209bfc87a8fd8c2cc4718156f
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-10-02 10:24:19 +0300

    Emacs package update

--- emacs/.emacs.d/elpa/async-20220928.1909/async-pkg.el
+++ emacs/.emacs.d/elpa/async-20220928.1909/async-pkg.el
@@ -1,4 +1,4 @@
-(define-package "async" "3.8.8" "Asynchronous processing in Emacs"
+(define-package "async" "20220928.1909" "Asynchronous processing in Emacs"
   '((emacs "24.4"))
   :commit "53addd02fbad33765f508f8e6254ebd44e1f81e5" :authors
   '(("John Wiegley" . "jwiegley@gmail.com"))
--- emacs/.emacs.d/package-quickstart.el
+++ emacs/.emacs.d/package-quickstart.el
@@ -10296,10 +10296,10 @@ and toggle it if ARG is `toggle'; disable the mode otherwise.
 
 
 )
-(let ((load-true-file-name "/Users/laurynas/.emacs.d/elpa/async-3.8.8/async-autoloads.el")(load-file-name "/Users/laurynas/.emacs.d/elpa/async-3.8.8/async-autoloads.el"))
+(let ((load-true-file-name "/Users/laurynas/.emacs.d/elpa/async-20220928.1909/async-autoloads.el")(load-file-name "/Users/laurynas/.emacs.d/elpa/async-20220928.1909/async-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/async-3.8.8/async-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/async-20220928.1909/async-autoloads.el") (car load-path))))
 
 
 
