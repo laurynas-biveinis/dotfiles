d1059b55c561b5dc2799bd2af03df6eb237bcc59
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-10-26 05:37:58 +0300

    Emacs package update

--- emacs/.emacs.d/elpa/projectile-2.5.0/projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from projectile.el  -*- no-byte-compile: t -*-
-(define-package "projectile" "2.5.0" "Manage and navigate projects in Emacs easily" '((emacs "25.1")) :commit "a2a1aba8aa12d0ff0e044c4336a5c2598c259720" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :keywords '("project" "convenience") :url "https://github.com/bbatsov/projectile")
---
+++ emacs/.emacs.d/elpa/projectile-2.6.0/projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from projectile.el  -*- no-byte-compile: t -*-
+(define-package "projectile" "2.6.0" "Manage and navigate projects in Emacs easily" '((emacs "25.1")) :commit "e3bae8c91bb69c501dcc2e74f9ea14e89cf0a09c" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.dev")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.dev") :keywords '("project" "convenience") :url "https://github.com/bbatsov/projectile")
