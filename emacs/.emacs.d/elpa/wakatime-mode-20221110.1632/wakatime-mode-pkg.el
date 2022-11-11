d390583de7964c9188f71b516b4be15b56689c09
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-11-11 05:37:54 +0200

    Emacs package update

---
+++ emacs/.emacs.d/elpa/wakatime-mode-20221110.1632/wakatime-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from wakatime-mode.el  -*- no-byte-compile: t -*-
+(define-package "wakatime-mode" "20221110.1632" "Automatic time tracking extension for WakaTime" 'nil :commit "ef923829912c3854d230834f81083814b7c9d992" :authors '(("Gabor Torok" . "gabor@20y.hu")) :maintainer '("Alan Hamlett" . "alan@wakatime.com") :keywords '("calendar" "comm"))
--- emacs/.emacs.d/package-quickstart.el
+++ emacs/.emacs.d/package-quickstart.el
@@ -470,10 +470,10 @@ Setup wgrep preparation.
 
 
 )
-(let ((load-true-file-name "/Users/laurynas/.emacs.d/elpa/wakatime-mode-20220621.616/wakatime-mode-autoloads.el")(load-file-name "/Users/laurynas/.emacs.d/elpa/wakatime-mode-20220621.616/wakatime-mode-autoloads.el"))
+(let ((load-true-file-name "/Users/laurynas/.emacs.d/elpa/wakatime-mode-20221110.1632/wakatime-mode-autoloads.el")(load-file-name "/Users/laurynas/.emacs.d/elpa/wakatime-mode-20221110.1632/wakatime-mode-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/wakatime-mode-20220621.616/wakatime-mode-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/Users/laurynas/.emacs.d/elpa/wakatime-mode-20221110.1632/wakatime-mode-autoloads.el") (car load-path))))
 
 
 
