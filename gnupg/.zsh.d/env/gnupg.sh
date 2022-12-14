d0c0233e079712db313adb68fe46191edb3b19cb
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-12-14 10:52:48 +0200

    Shell: minor cleanups for gpg-agent.conf linking

--- gnupg/.zsh.d/env/gnupg.sh
+++ gnupg/.zsh.d/env/gnupg.sh
@@ -2,13 +2,17 @@
 
 UNAME_OUT="$(uname -s)"
 
+TARGET=~/.gnupg/gpg-agent.conf
+
 if [ "$UNAME_OUT" = "Darwin" ]; then
     if [ "$(arch)" = "arm64" ]; then
-        ln -sf ~/.gnupg/gpg-agent.conf.macOS.apple-silicon \
-           ~/.gnupg/gpg-agent.conf
+        ln -sf ~/.gnupg/gpg-agent.conf.macOS.apple-silicon $TARGET
     else
-        ln -sf ~/.gnupg/gpg-agent.conf.macOS.intel ~/.gnupg/gpg-agent.conf
+        ln -sf ~/.gnupg/gpg-agent.conf.macOS.intel $TARGET
     fi
 else
-    ln -sf ~/.gnupg/gpg-agent.conf.linux ~/.gnupg/gpg-agent.conf
+    ln -sf ~/.gnupg/gpg-agent.conf.linux $TARGET
 fi
+
+unset TARGET
+unset UNAME_OUT
