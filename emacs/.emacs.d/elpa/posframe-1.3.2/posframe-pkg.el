77bf16d894363293f855df0c9041df21f305385e
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-12-22 06:21:37 +0200

    Emacs package update

--- emacs/.emacs.d/elpa/posframe-1.3.2/posframe-pkg.el
+++ emacs/.emacs.d/elpa/posframe-1.3.2/posframe-pkg.el
@@ -1,6 +1,6 @@
-(define-package "posframe" "1.3.1" "Pop a posframe (just a frame) at point"
+(define-package "posframe" "1.3.2" "Pop a posframe (just a frame) at point"
   '((emacs "26.1"))
-  :commit "030a63bc8d8023386998ea9a1ee0eb041330a80d" :authors
+  :commit "aa88860a16e28a311f81e18f1d9ed2e7d9e33991" :authors
   '(("Feng Shu" . "tumashu@163.com"))
   :maintainer
   '("Feng Shu" . "tumashu@163.com")
