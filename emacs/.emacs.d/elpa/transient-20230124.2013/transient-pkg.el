d7cd075f57a512c12397c242234e1eca02efe424
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-01-25 07:27:28 +0200

    Emacs package update

--- emacs/.emacs.d/elpa/transient-20230124.2013/transient-pkg.el
+++ emacs/.emacs.d/elpa/transient-20230124.2013/transient-pkg.el
@@ -1,7 +1,7 @@
-(define-package "transient" "20230120.1559" "Transient commands"
+(define-package "transient" "20230124.2013" "Transient commands"
   '((emacs "25.1")
     (compat "28.1.1.0"))
-  :commit "d800ce01e62c896f4aef8ad14a03f49f781c5b38" :authors
+  :commit "4a823dbf7f57f97877a519d12b949b5b202d517c" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
   '("Jonas Bernoulli" . "jonas@bernoul.li")
