3bb49977b93852a70c695188d4d8e5fcb3bf6b25
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-04-26 08:30:52 +0300

    Rust: no need to symlink rust-analyzer anymore

--- setup-macos.sh
+++ setup-macos.sh
@@ -381,4 +381,3 @@ duti -s jp.tmkk.XLD .flac all
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup component add rust-src
 rustup component add rust-analyzer
-ln -s `rustup which --toolchain stable rust-analyzer` ~/.cargo/bin
