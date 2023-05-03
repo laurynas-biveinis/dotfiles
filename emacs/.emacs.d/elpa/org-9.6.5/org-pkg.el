20f56788bbe8d9acc7a256b76d7d01fdcda0cc02
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-05-03 11:17:23 +0300

    Emacs package update

---
+++ emacs/.emacs.d/elpa/org-9.6.5/org-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from org.el  -*- no-byte-compile: t -*-
+(define-package "org" "9.6.5" "Outline-based notes management and organizer" '((emacs "26.1")) :commit "921e0e8bc1158736313db77c2ebc503b50211b89" :authors '(("Carsten Dominik" . "carsten.dominik@gmail.com")) :maintainer '("Bastien Guerry" . "bzg@gnu.org") :keywords '("outlines" "hypermedia" "calendar" "wp") :url "https://orgmode.org")
