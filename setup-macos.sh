c2341d7e9cd65acaf52ee4417f025a3d3ae36904
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-05-22 05:30:58 +0300

    macOS: Rust profiling

--- setup-macos.sh
+++ setup-macos.sh
@@ -381,3 +381,5 @@ duti -s jp.tmkk.XLD .flac all
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup component add rust-src
 rustup component add rust-analyzer
+rustup component add llvm-tools-preview
+cargo install grcov
