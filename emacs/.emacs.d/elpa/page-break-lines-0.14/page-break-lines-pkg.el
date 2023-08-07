b282d0563eac64b7a1b1f65c54ab906982167161
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-08-07 09:19:17 +0300

    Emacs: upgrade page-break-lines

--- emacs/.emacs.d/elpa/page-break-lines-0.14/page-break-lines-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/laurynas/.emacs.d/elpa/page-break-lines-0.14/page-break-lines.el  -*- no-byte-compile: t -*-
-(define-package "page-break-lines" "0.14" "Display ^L page breaks as tidy horizontal lines" '((emacs "24.4")) :commit "69caea070379f3324c530e96e06625c3cd097cb9" :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :keywords '("convenience" "faces") :url "https://github.com/purcell/page-break-lines")
---
+++ emacs/.emacs.d/elpa/page-break-lines-0.15/page-break-lines-pkg.el
@@ -0,0 +1,14 @@
+(define-package "page-break-lines" "0.15" "Display ^L page breaks as tidy horizontal lines"
+  '((emacs "24.4"))
+  :commit "c4283f580fa4feeb9abcaebb99709007db0a3159" :authors
+  '(("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainers
+  '(("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Steve Purcell" . "steve@sanityinc.com")
+  :keywords
+  '("convenience" "faces")
+  :url "https://github.com/purcell/page-break-lines")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
