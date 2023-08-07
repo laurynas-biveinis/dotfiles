72772ddebba1bd890eb7b8f505bbec3eadedbe9f
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-08-07 09:30:08 +0300

    Emacs: update packages

---
+++ emacs/.emacs.d/elpa/helm-3.9.3/helm-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm" "3.9.3" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.3")
+    (wfnames "1.1")
+    (popup "0.5.3"))
+  :commit "e347a2c98438e2dbb5b1f681729f00c0e816ffc6" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
