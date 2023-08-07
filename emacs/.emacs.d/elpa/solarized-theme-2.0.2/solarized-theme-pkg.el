e197c4667d1d4371ec94e6c9db772e8e0c94aa93
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-08-07 09:18:22 +0300

    Emacs: upgrade solarized-theme

--- emacs/.emacs.d/elpa/solarized-theme-2.0.2/solarized-theme-pkg.el
+++ emacs/.emacs.d/elpa/solarized-theme-2.0.2/solarized-theme-pkg.el
@@ -1,6 +1,6 @@
-(define-package "solarized-theme" "2.0.1" "The Solarized color theme"
+(define-package "solarized-theme" "2.0.2" "The Solarized color theme"
   '((emacs "24.1"))
-  :commit "0f0b1129cf51c85904f9e015da4fcca5f5f7a4ce" :authors
+  :commit "100542418b19859f9828114eeaed2be1071367bb" :authors
   '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
   :maintainers
   '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
