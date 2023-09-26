47a8dc4d257270c41c1e876114c0a737eb1500a1
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-09-26 17:04:25 +0300

    mysql-work: add MySQL 8.0.18 support

--- mysql-work/.bash.d/rc/mysql-work.sh
+++ mysql-work/.bash.d/rc/mysql-work.sh
@@ -31,14 +31,15 @@ UNAME_OUT="$(uname -s)"
 if [ "$UNAME_OUT" = "Darwin" ]; then
     if [ "$(arch)" = "arm64" ]; then
         BREW="/opt/homebrew/opt"
-        MY8026_28_EXTRA=("-DWITH_SSL=$BREW/openssl@1.1")
+        MY8018_28_EXTRA=("-DWITH_SSL=$BREW/openssl@1.1")
         MY8030_810_EXTRA=("-DWITH_DEVELOPER_ENTITLEMENTS=ON")
     else
-        MY8026_28_EXTRA=()
+        MY8018_28_EXTRA=()
         MY8030_810_EXTRA=()
     fi
-    MY8026_28_EXTRA+=("-DWITH_ICU=$BREW/icu4c")
-    MY8026_28_EXTRA_CXX_FLAGS=("-Wno-shadow-field"
+    MY8018_EXTRA=("-DWITH_ZSTD=bundled" "-DWITH_PROTOBUF=bundled")
+    MY8018_28_EXTRA+=("-DWITH_ICU=$BREW/icu4c")
+    MY8018_28_EXTRA_CXX_FLAGS=("-Wno-shadow-field"
                                "-Wno-unqualified-std-cast-call"
                                "-Wno-deprecated-copy-with-dtor"
                                "-Wno-bitwise-instead-of-logical"
@@ -53,23 +54,18 @@ if [ "$UNAME_OUT" = "Darwin" ]; then
     MARIA_COMMON=("-DCMAKE_C_FLAGS='-isystem /usr/local/include'"
                   "-DCMAKE_CXX_FLAGS='-isystem /usr/local/include'")
 
-    FB_COMMON=()
-    FB8028_EXTRA=("-DWITH_UNIT_TESTS=OFF")
-
     export MYCLANG12=("-DCMAKE_C_COMPILER=$BREW/llvm@12/bin/clang-12"
                       "-DCMAKE_CXX_COMPILER=$BREW/llvm@12/bin/clang++")
 else
-    MY8026_28_EXTRA=()
-    MY8026_28_EXTRA_CXX_FLAGS=("-Wno-unused-label")
+    MY8018_EXTRA=()
+    MY8018_28_EXTRA=()
+    MY8018_28_EXTRA_CXX_FLAGS=("-Wno-unused-label")
     MY8031_EXTRA_CXX_FLAGS=()
     MY8032_EXTRA_CXX_FLAGS=()
     MY8032_EXTRA=()
     MY8033_EXTRA_CXX_FLAGS=()
     MY8030_810_EXTRA=()
     MARIA_COMMON=()
-
-    FB_COMMON=("-DWITH_ZSTD=bundled" "-DWITH_PROTOBUF=bundled")
-    FB8028_EXTRA=("-DWITH_UNIT_TESTS=OFF")
 
     export MYCLANG12=("-DCMAKE_C_COMPILER=clang-12"
                       "-DCMAKE_CXX_COMPILER=clang++-12")
@@ -91,8 +87,8 @@ MY8R=("${CMAKE_RELEASE[@]}" "${MY8[@]}")
 unset MY8
 
 # Workaround Facebook tooling incompatibility with git worktrees
-FB_COMMON+=("-DMYSQL_GITHASH=0" "-DMYSQL_GITDATE=2100-02-29"
-            "-DROCKSDB_GITHASH=0" "-DROCKSDB_GITDATE=2100-02-29")
+FB_COMMON=("-DMYSQL_GITHASH=0" "-DMYSQL_GITDATE=2100-02-29"
+           "-DROCKSDB_GITHASH=0" "-DROCKSDB_GITDATE=2100-02-29")
 
 MARIA_COMMON+=("-DPLUGIN_MROONGA=NO" "-DPLUGIN_CONNECT=NO")
 
@@ -159,24 +155,24 @@ unset MY8029_CXX_FLAGS_RELEASE
 
 MY8027_28_EXTRA=("-DWITH_FIDO=bundled")
 
-# 8.0.28--8.0.26
+# 8.0.28--8.0.18
 
-MY8026_28_CXX_FLAGS_DEBUG=("${MY8026_28_EXTRA_CXX_FLAGS[@]}"
+MY8018_28_CXX_FLAGS_DEBUG=("${MY8018_28_EXTRA_CXX_FLAGS[@]}"
                            "-Wno-unknown-warning-option"
                            "-Wno-unused-but-set-variable"
                            "-Wno-discarded-qualifiers")
-MY8026_28_CXX_FLAGS_RELEASE=("${MY8026_28_EXTRA_CXX_FLAGS[@]}")
 
-MY8026_28_EXTRA+=("-DWITH_RAPIDJSON=bundled" "-DWITH_LZ4=bundled")
+MY8018_28_EXTRA+=("-DWITH_RAPIDJSON=bundled" "-DWITH_LZ4=bundled")
 
 # 8.0.28
 
-MY8028_CXX_FLAGS=("${MY8026_28_EXTRA_CXX_FLAGS[*]}")
+FB8028_EXTRA=("-DWITH_UNIT_TESTS=OFF")
+
+MY8028_CXX_FLAGS=("${MY8018_28_EXTRA_CXX_FLAGS[*]}")
 MY8028_CXX_FLAGS_DEBUG=("${MY8028_29_EXTRA_CXX_FLAGS_DEBUG[@]}"
-                        "${MY8026_28_CXX_FLAGS_DEBUG[*]}"
+                        "${MY8018_28_CXX_FLAGS_DEBUG[*]}"
                         "${CXX_FLAGS_DEBUG[@]}")
-MY8028_CXX_FLAGS_RELEASE=("${MY8026_28_CXX_FLAGS_RELEASE[*]}"
-                          "${CXX_FLAGS_RELEASE[@]}")
+MY8028_CXX_FLAGS_RELEASE=("${CXX_FLAGS_RELEASE[@]}")
 MY8028_EXTRA=("-DCMAKE_CXX_FLAGS=${MY8028_CXX_FLAGS[*]}"
               "-DCMAKE_C_FLAGS_DEBUG=${MY8028_CXX_FLAGS_DEBUG[*]}"
               "-DCMAKE_CXX_FLAGS_DEBUG=${MY8028_CXX_FLAGS_DEBUG[*]}"
@@ -188,10 +184,10 @@ unset MY8028_CXX_FLAGS_RELEASE
 
 # 8.0.27
 
-MY8027_CXX_FLAGS=("${MY8026_28_EXTRA_CXX_FLAGS[*]}")
-MY8027_CXX_FLAGS_DEBUG=("${MY8026_28_EXTRA_CXX_FLAGS[*]}"
+MY8027_CXX_FLAGS=("${MY8018_28_EXTRA_CXX_FLAGS[*]}")
+MY8027_CXX_FLAGS_DEBUG=("${MY8018_28_EXTRA_CXX_FLAGS[*]}"
                         "${CXX_FLAGS_DEBUG[@]}")
-MY8027_CXX_FLAGS_RELEASE=("${MY8026_28_EXTRA_CXX_FLAGS[*]}"
+MY8027_CXX_FLAGS_RELEASE=("${MY8018_28_EXTRA_CXX_FLAGS[*]}"
                           "${CXX_FLAGS_RELEASE[@]}")
 MY8027_EXTRA=("-DCMAKE_CXX_FLAGS=${MY8027_CXX_FLAGS[*]}"
               "-DCMAKE_C_FLAGS_DEBUG=${MY8027_CXX_FLAGS_DEBUG[*]}"
@@ -203,23 +199,42 @@ unset MY8027_CXX_FLAGS_RELEASE
 
 # 8.0.26
 
-MY8026_CXX_FLAGS=("${MY8026_28_EXTRA_CXX_FLAGS[*]}")
-MY8026_CXX_FLAGS_DEBUG=("${MY8026_28_EXTRA_CXX_FLAGS[*]}"
+MY8026_CXX_FLAGS=("${MY8018_28_EXTRA_CXX_FLAGS[*]}")
+MY8026_CXX_FLAGS_DEBUG=("${MY8018_28_EXTRA_CXX_FLAGS[*]}"
                         "${CXX_FLAGS_DEBUG[@]}")
-MY8026_CXX_FLAGS_RELEASE=("${MY8026_28_EXTRA_CXX_FLAGS[*]}"
+MY8026_CXX_FLAGS_RELEASE=("${MY8018_28_EXTRA_CXX_FLAGS[*]}"
                           "${CXX_FLAGS_RELEASE[@]}")
 MY8026_EXTRA=("-DCMAKE_CXX_FLAGS=${MY8026_CXX_FLAGS[*]}"
               "-DCMAKE_C_FLAGS_DEBUG=${MY8026_CXX_FLAGS_DEBUG[*]}"
               "-DCMAKE_CXX_FLAGS_DEBUG=${MY8026_CXX_FLAGS_DEBUG[*]}"
               "-DCMAKE_CXX_FLAGS_RELEASE=${MY8026_CXX_FLAGS_RELEASE[*]}")
 unset MY8026_28_C_FLAGS_DEBUG
-unset MY8026_28_CXX_FLAGS_DEBUG
-unset MY8026_28_CXX_FLAGS_RELEASE
-unset MY8026_28_EXTRA_CXX_FLAGS
-unset CXX_FLAGS_DEBUG
-unset CXX_FLAGS_RELEASE
 
 MY8026_EXTRA=("-DENABLE_DOWNLOADS=ON")
+
+# 8.0.18
+
+MY8018_EXTRA_CXX_FLAGS=("-Wno-deprecated-declarations" "-Wno-unused-result"
+                        "-Wno-range-loop-construct"
+                        "-Wno-non-c-typedef-for-linkage")
+
+MY8018_CXX_FLAGS=("${MY8018_28_EXTRA_CXX_FLAGS[*]}"
+                  "${MY8018_EXTRA_CXX_FLAGS[*]}")
+MY8026_CXX_FLAGS_DEBUG=("${MY8018_28_EXTRA_CXX_FLAGS[*]}"
+                        "${MY8018_28_CXX_FLAGS_DEBUG[*]}"
+                        "${MY8018_EXTRA_CXX_FLAGS[*]}" "${CXX_FLAGS_DEBUG[@]}")
+MY8018_CXX_FLAGS_RELEASE=("${MY8018_28_EXTRA_CXX_FLAGS[*]}"
+                          "${MY8018_EXTRA_CXX_FLAGS[*]}"
+                          "${CXX_FLAGS_RELEASE[@]}")
+MY8018_EXTRA+=("-DCMAKE_CXX_FLAGS=${MY8026_CXX_FLAGS[*]}"
+               "-DCMAKE_C_FLAGS_DEBUG=${MY8026_CXX_FLAGS_DEBUG[*]}"
+               "-DCMAKE_CXX_FLAGS_DEBUG=${MY8026_CXX_FLAGS_DEBUG[*]}"
+               "-DCMAKE_CXX_FLAGS_RELEASE=${MY8026_CXX_FLAGS_RELEASE[*]}")
+
+unset MY8018_28_CXX_FLAGS_DEBUG
+unset MY8018_28_EXTRA_CXX_FLAGS
+unset CXX_FLAGS_DEBUG
+unset CXX_FLAGS_RELEASE
 
 # Paydirt!
 
@@ -247,9 +262,9 @@ unset MY8030_810_EXTRA
 
 export MY8029D=("${MY8D[@]}" "${MY8029_EXTRA[@]}")
 
-export MY8028=("${MY8R[@]}" "${MY8028_EXTRA[@]}" "${MY8026_28_EXTRA[@]}"
+export MY8028=("${MY8R[@]}" "${MY8028_EXTRA[@]}" "${MY8018_28_EXTRA[@]}"
                "${MY8027_28_EXTRA[@]}")
-export MY8028D=("${MY8D[@]}" "${MY8028_EXTRA[@]}" "${MY8026_28_EXTRA[@]}"
+export MY8028D=("${MY8D[@]}" "${MY8028_EXTRA[@]}" "${MY8018_28_EXTRA[@]}"
                 "${MY8027_28_EXTRA[@]}")
 unset MY8028_29_EXTRA
 unset MY8028_EXTRA
@@ -258,14 +273,18 @@ export FB8028=("${MY8028[@]}" "${FB_COMMON[@]}" "${FB8028_EXTRA[@]}")
 export FB8028D=("${MY8028D[@]}" "${FB_COMMON[@]}" "${FB8028_EXTRA[@]}")
 unset FB8028_EXTRA
 
-export MY8027D=("${MY8D[@]}" "${MY8027_EXTRA[@]}" "${MY8026_28_EXTRA[@]}"
+export MY8027D=("${MY8D[@]}" "${MY8027_EXTRA[@]}" "${MY8018_28_EXTRA[@]}"
                 "${MY8027_28_EXTRA[@]}")
 unset MY8027_28_EXTRA
 
-export MY8026=("${MY8R[@]}" "${MY8026_EXTRA[@]}" "${MY8026_28_EXTRA[@]}")
+export MY8026=("${MY8R[@]}" "${MY8026_EXTRA[@]}" "${MY8018_28_EXTRA[@]}")
 export MY8026D=("${MY8D[@]}" "-DDEBUG_EXTNAME=OFF" "${MY8026_EXTRA[@]}"
-                "${MY8026_28_EXTRA[@]}")
-unset MY8026_28_EXTRA
+                "${MY8018_28_EXTRA[@]}")
+
+export MY8018=("${MY8R[@]}" "${MY8018_EXTRA[@]}" "${MY8018_28_EXTRA[@]}")
+export MY8018D=("${MY8D[@]}" "${MY8018_EXTRA[@]}" "${MY8018_28_EXTRA[@]}")
+
+unset MY8018_28_EXTRA
 unset MY8D
 unset MY8R
 
@@ -309,24 +328,90 @@ rm_tmp_mtr() {
 
 mysql_cmake() {
     if [ -f ../MYSQL_VERSION ]; then
-        major_ver_str=$(grep MYSQL_VERSION_MAJOR ../MYSQL_VERSION)
-        major_ver="${major_ver_str//[^0-9]/}"
-        if [ "$major_ver" != 8 ]; then
-            echo "Only MySQL version 8 is supported"
-            return
-        fi
-        minor_ver_str=$(grep MYSQL_VERSION_MINOR ../MYSQL_VERSION)
-        minor_ver="${minor_ver_str//[^0-9]/}"
-        patch_level_str=$(grep MYSQL_VERSION_PATCH ../MYSQL_VERSION)
-        patch_level="${patch_level_str//[^0-9]/}"
-
+        version_file="../MYSQL_VERSION"
         if [ -d ../rocksdb ]; then
+            flavor="facebook"
+        else
+            flavor="mysql"
+        fi
+    elif [ -f ../VERSION ]; then
+        version_file="../VERSION"
+        server_maturity=$(grep SERVER_MATURITY ../VERSION)
+        if [ -z $server_maturity ]; then
+            flavor="mysql"
+        else
+            flavor="mariadb"
+        fi
+    else
+        echo "Neither MYSQL_VERSION nor VERSION found"
+        return
+    fi
+
+    if [ -z $flavor ]; then
+        echo "Unknown source tree flavor"
+        return
+    fi
+
+    major_ver_str=$(grep MYSQL_VERSION_MAJOR $version_file)
+    major_ver="${major_ver_str//[^0-9]/}"
+    minor_ver_str=$(grep MYSQL_VERSION_MINOR $version_file)
+    minor_ver="${minor_ver_str//[^0-9]/}"
+    patch_level_str=$(grep MYSQL_VERSION_PATCH $version_file)
+    patch_level="${patch_level_str//[^0-9]/}"
+
+    case "$flavor" in
+        "mysql")
+            echo "Configuring MySQL $major_ver.$minor_ver.$patch_level"
+            case "$major_ver.$minor_ver.$patch_level" in
+                8.1.0)
+                    release_flags=("${MY810[@]}")
+                    debug_flags=("${MY810D[@]}")
+                    ;;
+                8.0.34)
+                    release_flags=("${MY8034[@]}")
+                    debug_flags=("${MY8034D[@]}")
+                    ;;
+                8.0.33)
+                    release_flags=("${MY8033[@]}")
+                    debug_flags=("${MY8033D[@]}")
+                    ;;
+                8.0.32)
+                    release_flags=("${MY8032[@]}")
+                    debug_flags=("${MY8032D[@]}")
+                    ;;
+                8.0.31)
+                    release_flags=("${MY8031[@]}")
+                    debug_flags=("${MY8031D[@]}")
+                    ;;
+                8.0.30)
+                    release_flags=("${MY8030[@]}")
+                    debug_flags=("${MY8030D[@]}")
+                    ;;
+                8.0.29)
+                    release_flags=("${MY8029[@]}")
+                    debug_flags=("${MY8029D[@]}")
+                    ;;
+                8.0.28)
+                    release_flags=("${MY8028[@]}")
+                    debug_flags=("${MY8028D[@]}")
+                    ;;
+                8.0.18)
+                    release_flags=("${MY8018[@]}")
+                    debug_flags=("${MY8018D[@]}")
+                    ;;
+                *)
+                    echo "Unsupported version, please implement"
+                    return
+                    ;;
+            esac
+            ;;
+        "facebook")
             echo "Configuring Facebook MySQL $major_ver.$minor_ver.$patch_level"
-            case "$minor_ver.$patch_level" in
-                0.32)
+            case "$major_ver.$minor_ver.$patch_level" in
+                8.0.32)
                     debug_flags=("${FB8032D[@]}")
                     ;;
-                0.28)
+                8.0.28)
                     release_flags=("${FB8028[@]}")
                     debug_flags=("${FB8028D[@]}")
                     ;;
@@ -335,53 +420,16 @@ mysql_cmake() {
                     return
                     ;;
             esac
-        else
-            echo "Configuring MySQL $major_ver.$minor_ver.$patch_level"
-            case "$minor_ver.$patch_level" in
-                1.0)
-                    release_flags=("${MY810[@]}")
-                    debug_flags=("${MY810D[@]}")
-                    ;;
-                0.34)
-                    release_flags=("${MY8034[@]}")
-                    debug_flags=("${MY8034D[@]}")
-                    ;;
-                0.33)
-                    release_flags=("${MY8033[@]}")
-                    debug_flags=("${MY8033D[@]}")
-                    ;;
-                0.32)
-                    release_flags=("${MY8032[@]}")
-                    debug_flags=("${MY8032D[@]}")
-                    ;;
-                0.31)
-                    release_flags=("${MY8031[@]}")
-                    debug_flags=("${MY8031D[@]}")
-                    ;;
-                0.30)
-                    release_flags=("${MY8030[@]}")
-                    debug_flags=("${MY8030D[@]}")
-                    ;;
-                0.29)
-                    release_flags=("${MY8029[@]}")
-                    debug_flags=("${MY8029D[@]}")
-                    ;;
-                0.28)
-                    release_flags=("${MY8028[@]}")
-                    debug_flags=("${MY8028D[@]}")
-                    ;;
-                *)
-                    echo "Unsupported version, please add"
-                    return
-                    ;;
-            esac
-        fi
-    elif [ -f ../VERSION ]; then
-        echo "Configuring MariaDB, not implemented yet"
-    else
-        echo "Neither MariaDB nor MySQL source tree"
-        return
-    fi
+            ;;
+        "mariadb")
+            echo "MariaDB unsupported, please add"
+            return
+            ;;
+        *)
+            echo "Unsupported flavor, should have caught sooner"
+            return
+            ;;
+    esac
 
     build_dir="$(basename "$PWD")"
 
@@ -432,7 +480,6 @@ mysql_cmake() {
             cmake -G Ninja .. "${release_flags[@]}" "$@"
             ;;
     esac
-    set +x
 }
 
 mysql_build() {
