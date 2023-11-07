ea6547e134b6d0df8a55d8c33baba8e454050d99
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-11-07 13:47:37 +0200

    Emacs .dir-locals.el for MySQL: make ispell-personal-dictionary buffer-local
    Otherwise it is set globally, which is totally not what I want.

--- mysql-work/vilniusdb/.dir-locals.el
+++ mysql-work/vilniusdb/.dir-locals.el
@@ -1,10 +1,10 @@
 ;; StackOverflow'ed from https://stackoverflow.com/a/11474657/80458
 ;; An alternative would be to use `projectile-project-root', but the current
 ;; method works without Projectile too.
-((nil . ((eval . (setq ispell-personal-dictionary
-                       (expand-file-name
-                        ".ispell.dict"
-                        (file-name-directory
-                         (let ((d (dir-locals-find-file "./")))
-                           (if (stringp d) d (car d)))))))))
+((nil . ((eval . (setq-local ispell-personal-dictionary
+                             (expand-file-name
+                              ".ispell.dict"
+                              (file-name-directory
+                               (let ((d (dir-locals-find-file "./")))
+                                 (if (stringp d) d (car d)))))))))
  (c++-mode . ((c-file-style . "google"))))
