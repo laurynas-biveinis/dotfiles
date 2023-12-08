81612e80a99e8de0faed28c4d2f2d7b0cdfbc7cc
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-12-08 07:13:22 +0200

    Ubuntu 23.10 installation

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-sudo locale-gen en_US.utf8
-sudo update-locale
-
 # Not actually meant for running as an invoked script, but rather as a sequence
 # of steps to consult and copy and paste into the terminal, and to do some
 # unscripted steps manually.
+
+sudo apt-get install locales
+sudo locale-gen en_US.utf8
+sudo update-locale
 
 sudo apt-get install net-tools
 
@@ -19,15 +20,15 @@ sudo apt-get install stow tldr git-doc diffutils-doc perl-doc make \
      gnu-standards autoconf-doc gettext bison-doc flex-doc gdb-doc gettext-doc \
      libtool-doc m4-doc cmake cmake-doc diffstat unzip pinentry-doc zip \
      software-properties-common colordiff valgrind linux-tools-generic \
-     libjemalloc2 python3-scipy python-numpy-doc python3-pytest \
-     python-scipy-doc unattended-upgrades screen fzf fd-find clang moreutils \
-     psmisc zsh zsh-doc libjemalloc-dev bzip2-doc valgrind-dbg hexyl ripgrep \
-     icu-doc ncurses-doc pkg-config lcov cpufrequtils libboost-dev \
-     libboost-doc cppcheck iwyu clang-12 clang-12-doc llvm-12-doc lld-12 \
-     clang-11 clang-11-doc llvm-11-doc lld-11 cpuset python3-pip g++-12 \
-     gcc-12-doc libstdc++-12-doc g++-10 gcc-10-doc clang-14-doc ninja-build \
-     cmake-format clang-tidy neovim man-db lynis shellcheck htop jq bat \
-     cpplint duf exa
+     libjemalloc2 python3-scipy python3-pytest python-scipy-doc \
+     unattended-upgrades screen fzf fd-find clang moreutils psmisc zsh zsh-doc \
+     libjemalloc-dev bzip2-doc hexyl ripgrep icu-doc ncurses-doc pkg-config \
+     lcov cpufrequtils libboost-dev libboost-doc cppcheck iwyu cpuset \
+     python3-pip g++-12 gcc-12-doc libstdc++-12-doc g++-10 gcc-10-doc \
+     clang-14-doc ninja-build cmake-format clang-tidy neovim man-db lynis \
+     shellcheck htop jq bat cpplint duf exa clang-16-doc clang-17 clang-17-doc \
+     clang-format-17 clang-tidy-17 clang-tools-17 clangd-17 lld-17 llvm-17 \
+     llvm-17-doc llvm-17-tools python3-pandas pipx
 # DeepState
 sudo apt-get install libc6-dev-i386
 # Not found on AWS EC2
@@ -37,16 +38,8 @@ sudo ln -sf /usr/bin/fdfind /usr/local/bin/fd
 # So that I can have "gsed" as a GNU Sed on any platform
 sudo ln -sf /bin/sed /bin/gsed
 # sudo usermod -aG docker laurynas
-# LLVM
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-echo 'deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-17 main' \
-    | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install clang-17 libomp5-17 llvm-17 lld-17 clang-tools-17 \
-     clang-tidy-17 clang-17-doc llvm-17-doc clang-format-17
-pip install pandas
 # MySQL development specific
-sudo apt-get install ccache rapidjson-dev ncdu libaio-dev libssl-dev \
+sudo apt-get install rapidjson-dev ncdu libaio-dev libssl-dev \
      libreadline-dev readline-doc liblz4-dev libre2-dev libicu-dev zlib1g-dev \
      libevent-dev libcurl4-gnutls-dev libcurl4-doc libpam0g-dev libtirpc-dev \
      libprotobuf-dev libldap2-dev libsasl2-dev libnuma-dev mecab libprotoc-dev \
