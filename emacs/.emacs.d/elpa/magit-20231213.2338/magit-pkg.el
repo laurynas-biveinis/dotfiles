d6b97021b2fa8c1f54bbc686edb08935c11928a5
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-12-14 08:23:23 +0200

    Emacs package update

--- emacs/.emacs.d/elpa/magit-20231213.2338/magit-pkg.el
+++ emacs/.emacs.d/elpa/magit-20231213.2338/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20231213.48" "A Git porcelain inside Emacs."
+(define-package "magit" "20231213.2338" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (compat "29.1.4.4")
     (dash "20221013")
@@ -7,7 +7,7 @@
     (seq "2.24")
     (transient "20231204")
     (with-editor "20230917"))
-  :commit "0cc9cd067e6b3333cfa08098f47486d4e5a8afab" :authors
+  :commit "eac5c51fe002d8d1415b8ad97cb5b543c2051b15" :authors
   '(("Marius Vollmer" . "marius.vollmer@gmail.com")
     ("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
