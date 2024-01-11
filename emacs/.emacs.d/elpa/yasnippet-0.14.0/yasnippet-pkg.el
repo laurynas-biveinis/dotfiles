8719a1f2eb14ca069cebe65918b36596dfb24f55
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-01-11 22:08:28 +0200

    Emacs: reinstall yasnippet
    Lost in the great package cleanup

---
+++ emacs/.emacs.d/elpa/yasnippet-0.14.0/yasnippet-pkg.el
@@ -0,0 +1,12 @@
+(define-package "yasnippet" "0.14.0" "Yet another snippet extension for Emacs"
+  '((cl-lib "0.5"))
+  :commit "3bf9a3b1af37174a004798b7195826af0123fa6a" :maintainers
+  '(("Noam Postavsky" . "npostavs@gmail.com"))
+  :maintainer
+  '("Noam Postavsky" . "npostavs@gmail.com")
+  :keywords
+  '("convenience" "emulation")
+  :url "http://github.com/joaotavora/yasnippet")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
