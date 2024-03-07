f8d7b6ed91a11bc3dab4e7a68d830971e43adff0
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-03-07 17:58:06 +0200

    macOS: install Emacs with XWidgets support

--- setup-macos-emacs.sh
+++ setup-macos-emacs.sh
@@ -2,7 +2,7 @@
 
 brew tap railwaycat/emacsmacport
 brew install emacs-mac --with-native-compilation --with-unlimited-select \
-     --with-librsvg --with-mac-metal  --with-natural-title-bar
+     --with-librsvg --with-mac-metal --with-natural-title-bar --with-xwidgets
 # Intel
 osascript -e \
           'tell application "Finder" to make alias file to POSIX file "/usr/local/opt/emacs-mac/Emacs.app" at POSIX file "/Applications"'
