c2da4c6910d2cb651e429b1e788f2470be8cb3a6
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-03-21 22:00:59 +0200

    Emacs: new helpers in my-lib.el

--- emacs/.emacs.d/my/my-lib.el
+++ emacs/.emacs.d/my/my-lib.el
@@ -6,6 +6,17 @@
 
 ;;; Code:
 
+(defconst dotfiles--muspy-album-release-date-and-title
+  "\\([0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}\\): \\(.*? - .*?\\) (Album)")
+
+(defconst dotfiles--gh-pr-in-subject "^.*\(PR #\\([0-9]+\\)\)$")
+(defconst dotfiles--gh-url-prefix "https://github.com/")
+(defconst dotfiles--gh-closed-pr-url-format
+  (concat "Closed .*\\(" dotfiles--gh-url-prefix ".*/.*/pull/%s\\)"))
+(defconst dotfiles--gh-org-and-project
+  (concat dotfiles--gh-url-prefix "\\(.*\\)/pull/[0-9]+"))
+(defconst dotfiles--gh-repo "\\(https://github.com/.*/.*\\)/pull/[0-9]+")
+
 (require 'seq)
 (require 'mu4e-mime-parts)
 
@@ -28,6 +39,51 @@
          (file-path (mu4e-join-paths base-dir (plist-get part :filename))))
     (mm-save-part-to-file (plist-get part :handle) file-path)
     file-path))
+
+(require 'org-element)
+
+(defun dotfiles--org-headline-has-url (headline url)
+  "Return the HEADLINE if it has the URL property with the given value."
+  (let ((url-property-value (org-element-property :URL headline)))
+    (and url-property-value (string= url url-property-value)
+         headline)))
+
+(defun dotfiles--find-org-node-with-url-property-in-buffer (url)
+  "Find an Org node with a given URL property value in the current buffer."
+  (org-element-map (org-element-parse-buffer) 'headline
+    (lambda (headline)
+      (dotfiles--org-headline-has-url headline url)) nil t))
+
+(defun dotfiles--find-org-node-with-url-property (url)
+  "Find the Org node with a given URL property value across `org-agenda-files'."
+  (let ((files (org-agenda-files))
+        (found nil))
+    (while (and files (not found))
+      (let* ((file (pop files))
+             (buffer (or (find-buffer-visiting file)
+                         (find-file-noselect file t)))
+             (node (with-current-buffer buffer
+                     (dotfiles--find-org-node-with-url-property-in-buffer url))))
+        (when node
+          (setq found (list :buffer buffer :headline node)))))
+    found))
+
+(require 'org-clock)
+
+(defun dotfiles--with-different-org-clock (func &rest args)
+  "Save the current org clock, clock-in, call FUNC with ARGS, restore clock.
+
+The marker must be at the new clock position."
+  (let ((current-clock-marker (when (org-clocking-p)
+                                (copy-marker org-clock-marker))))
+    (unwind-protect
+        (progn
+          (org-clock-in)
+          (apply func args))
+      (if current-clock-marker
+          (org-with-point-at current-clock-marker
+            (org-clock-in))
+        (org-clock-out)))))
 
 (provide 'my-lib)
 ;;; my-lib.el ends here
