bd67ae5482855d5e69f99d7514735578d0aba93b
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-04-15 17:14:06 +0300

    macOS for MySQL work: brew install openblas

--- setup-macos-mysql-work.sh
+++ setup-macos-mysql-work.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 brew install ghostscript bison libfido2 doxygen graphviz lz4 libeatmydata \
-     rapidjson protobuf@21 llvm@16
+     rapidjson protobuf@21 llvm@16 openblas
 brew link protobuf@21
 sudo ln -sf /opt/homebrew/opt/llvm@16/bin/clang-format \
      /usr/local/bin/clang-format
