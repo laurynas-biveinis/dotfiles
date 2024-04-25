8d96ccc101a8ae88fbedeb21b8191942949ae6dc
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-04-25 07:07:40 +0300

    macOS: tweak MySQL development installation instructions

--- setup-macos-mysql-work.sh
+++ setup-macos-mysql-work.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 brew install ghostscript bison libfido2 doxygen graphviz lz4 libeatmydata \
-     rapidjson protobuf@21 llvm@16 openblas
+     rapidjson protobuf@21 llvm@16 openblas llvm@17
 brew link protobuf@21
-sudo ln -sf /opt/homebrew/opt/llvm@16/bin/clang-format \
-     /usr/local/bin/clang-format
+sudo ln -sf /opt/homebrew/opt/gcc/lib/gcc/current/libgfortran.a \
+     `xcode-select -p`/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/libgfortran.a
