aa15b9405dc602cd950aa33a601ce1a3245f4dda
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-05-10 21:48:04 +0300

    Suppress a pylint warning

--- scripts/usr/lib/python/common.py
+++ scripts/usr/lib/python/common.py
@@ -4,6 +4,7 @@
 class QIFInvestmentTransaction:
     """An investment transaction in QIF format."""
 
+    # pylint: disable-next=too-many-arguments
     def __init__(self, date, security, fee, amount, unit_price, count):
         self._date = date
         self._security = security
