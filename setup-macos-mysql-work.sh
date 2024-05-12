5b67955b697505900816c2d080ba0388d06521f1
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-05-12 20:20:59 +0300

    macOS / MySQL: install llvm@14

--- setup-macos-mysql-work.sh
+++ setup-macos-mysql-work.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 brew install ghostscript bison libfido2 doxygen graphviz lz4 libeatmydata \
-     rapidjson protobuf@21 llvm@16 openblas llvm@17
+     rapidjson protobuf@21 llvm@16 openblas llvm@17 llvm@14
 brew link protobuf@21
 sudo ln -sf /opt/homebrew/opt/gcc/lib/gcc/current/libgfortran.a \
      `xcode-select -p`/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/libgfortran.a
