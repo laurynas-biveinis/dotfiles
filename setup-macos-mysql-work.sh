bb2bc89d4fe9aa362cd8e2254647f2ff17e39430
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-05-14 11:53:34 +0300

    macOS / MySQL: add a note on libgfortran.a symlink

--- setup-macos-mysql-work.sh
+++ setup-macos-mysql-work.sh
@@ -3,5 +3,6 @@
 brew install ghostscript bison libfido2 doxygen graphviz lz4 libeatmydata \
      rapidjson protobuf@21 llvm@16 openblas llvm@17 llvm@14
 brew link protobuf@21
+# Needs to be re-executed after every XCode update
 sudo ln -sf /opt/homebrew/opt/gcc/lib/gcc/current/libgfortran.a \
      `xcode-select -p`/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/libgfortran.a
