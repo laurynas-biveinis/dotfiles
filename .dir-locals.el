01b35b0aa50944d49d5eadb21c1cbb81f530267f
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-05-15 22:19:01 +0300

    Configure dotfiles TODO highlighting

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((magit-status-mode . ((magit-todos-exclude-globs . ("emacs/.emacs.d/abbrev_defs"
+                                                     "emacs/.emacs.d/elpa/*"
+                                                     "zsh/.p10k.zsh")))))
