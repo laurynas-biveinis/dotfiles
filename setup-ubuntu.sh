621ae80382c7af0d0f8e068be1fc45e8888ba51c
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-06-10 17:58:51 +0300

    Split out setup-ubuntu-mysql-work.sh out of setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -44,15 +44,6 @@ sudo ln -sf /usr/bin/fdfind /usr/local/bin/fd
 # So that I can have "gsed" as a GNU Sed on any platform
 sudo ln -sf /bin/sed /bin/gsed
 # sudo usermod -aG docker laurynas
-# MySQL development specific
-sudo apt-get install rapidjson-dev ncdu libaio-dev libssl-dev \
-	libreadline-dev readline-doc liblz4-dev libre2-dev libicu-dev zlib1g-dev \
-	libevent-dev libcurl4-gnutls-dev libcurl4-doc libpam0g-dev libtirpc-dev \
-	libprotobuf-dev libldap2-dev libsasl2-dev libnuma-dev mecab libprotoc-dev \
-	doxygen doxygen-doc graphviz graphviz-doc libedit-dev libgcrypt20-dev \
-	libfido2-dev libssl-doc rapidjson-doc libeatmydata1 libudev-dev \
-	libzstd-dev protobuf-compiler libsasl2-dev libsasl2-modules-gssapi-mit \
-	libkrb5-dev
 
 # For CPU-intensive benchmarks
 sudo sh -c "echo -1 > /proc/sys/kernel/perf_event_paranoid"
