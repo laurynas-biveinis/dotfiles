873922954d75bc9261ec8e5a14b0a1e13610b7c3
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-09-04 21:39:03 +0300

    Emacs: fix parse errors, FFS

--- emacs/.emacs.d/my/my-org-gtd-test.el
+++ emacs/.emacs.d/my/my-org-gtd-test.el
@@ -99,7 +99,7 @@
 (ert-deftest my-org-gtd-org-use-tag-inheritance-t ()
   "Test `org-use-tag-inheritance' when it's t."
   (let ((org-use-tag-inheritance t)
-        (org-todo-keywords '((sequence "TODO(t!)" "KILL(k!)")))
+        (org-todo-keywords '((sequence "TODO(t!)" "KILL(k!)"))))
     (my-org-gtd-initialize)
     (should (equal org-use-tag-inheritance t))))
 
@@ -107,7 +107,7 @@
   "Test `org-use-tag-inheritance' matching `my-org-gtd-somedaymaybe-tag'."
   (let ((org-use-tag-inheritance "may.*")
         (my-org-gtd-somedaymaybe-tag "maybe")
-        (org-todo-keywords '((sequence "TODO(t!)" "KILL(k!)")))
+        (org-todo-keywords '((sequence "TODO(t!)" "KILL(k!)"))))
     (my-org-gtd-initialize)
     (should (equal org-use-tag-inheritance "may.*"))))
 
@@ -121,7 +121,7 @@
   "Test adding `my-org-gtd-somedaymaybe-tag' to `org-use-tag-inheritance'."
   (let ((org-use-tag-inheritance '("foo" "bar"))
         (my-org-gtd-somedaymaybe-tag "somedaymaybe")
-        (org-todo-keywords '((sequence "TODO(t!)" "KILL(k!)")))
+        (org-todo-keywords '((sequence "TODO(t!)" "KILL(k!)"))))
     (my-org-gtd-initialize)
     (should (equal org-use-tag-inheritance '("somedaymaybe" "foo" "bar")))))
 
