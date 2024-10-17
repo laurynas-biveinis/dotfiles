3fcc174eeae6bb083fa9146dde9d061afb256c5a
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-10-17 14:05:52 +0300

    setup-ubuntu.sh: updates for 24.10

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -4,6 +4,7 @@
 # of steps to consult and copy and paste into the terminal, and to do some
 # unscripted steps manually.
 
+# Not needed in the minimal install:
 sudo apt-get install locales
 sudo locale-gen en_US.utf8
 sudo update-locale
@@ -15,29 +16,35 @@ sudo apt-get install net-tools
 # Common
 sudo apt-get update
 sudo apt-get dist-upgrade
+# Not needed in the minimal install:
+sudo apt-get install manpages-dev binutils libc6-dev \
+     software-properties-common unattended-upgrades screen psmisc man-db htop \
+     jq strace lsof
 sudo apt-get install stow tldr git-doc diffutils-doc perl-doc make \
-	build-essential gdb manpages-dev binutils binutils-doc cpp g++ gcc \
-	libasan5 liblsan0 libtsan0 libubsan1 libc6-dev cpp-doc gcc-doc autoconf \
-	automake libtool flex bison glibc-doc make-doc autoconf-archive \
-	gnu-standards autoconf-doc gettext bison-doc flex-doc gdb-doc \
-	gettext-doc libtool-doc m4-doc cmake cmake-doc diffstat unzip \
-	pinentry-doc zip software-properties-common colordiff valgrind \
-	linux-tools-generic libjemalloc2 python3-scipy python3-pytest \
-	python-scipy-doc unattended-upgrades screen fzf fd-find clang \
-	moreutils psmisc zsh zsh-doc libjemalloc-dev bzip2-doc hexyl ripgrep \
-	icu-doc ncurses-doc pkg-config lcov cpufrequtils libboost-dev \
-	libboost-doc cppcheck iwyu cpuset python3-pip ninja-build cmake-format \
-	clang-tidy neovim man-db lynis shellcheck htop jq bat cpplint duf eza \
-	python3-pandas pipx strace lsof libstdc++-doc pipx libgfortran-dev
+     build-essential gdb binutils-doc cpp g++ gcc liblsan0 libtsan0 libubsan1 \
+     cpp-doc gcc-doc autoconf automake libtool flex bison glibc-doc make-doc \
+     autoconf-archive gnu-standards autoconf-doc gettext bison-doc flex-doc \
+     gdb-doc gettext-doc libtool-doc m4-doc cmake cmake-doc diffstat unzip \
+     pinentry-doc zip colordiff valgrind linux-tools-generic libjemalloc2 \
+     python3-scipy python3-pytest python-scipy-doc fzf fd-find clang moreutils \
+     zsh zsh-doc libjemalloc-dev bzip2-doc hexyl ripgrep icu-doc ncurses-doc \
+     pkg-config lcov cpufrequtils libboost-dev libboost-doc cppcheck iwyu \
+     cpuset python3-pip ninja-build cmake-format clang-tidy neovim lynis \
+     shellcheck bat cpplint duf eza python3-pandas pipx pipx
 # Compiler versions
-sudo apt-get install g++-10 gcc-10-doc libgfortran-10-dev
-sudo apt-get install g++-11 gcc-11-doc libgfortran-11-dev
-sudo apt-get install g++-12 cpp-12-doc gcc-12-doc libgfortran-12-dev
-# Default in 24.04
-sudo apt-get install libstdc++-13-doc
-# Absent in 24.04
+# Absent in 24.10
+sudo apt-get install g++-10 libgfortran-10-dev
+sudo apt-get install g++-11 libgfortran-11-dev
+sudo apt-get install g++-12 libgfortran-12-dev
+sudo apt-get install g++-13 libgfortran-13-dev
+sudo apt-get install g++-14 libgfortran-14-dev cpp-14-doc gcc-14-doc \
+     libstdc++-14-doc
+# Absent in 24.04+
 sudo apt-get install clang-12 clang-12-doc clang-tools-12 lld-12 llvm-12 \
-	llvm-12-doc llvm-12-tools
+     llvm-12-doc llvm-12-tools
+# Absent in 24.10
+sudo apt-get install clang-13 clang-13-doc clang-tools-13 lld-13 llvm-13 \
+     llvm-13-doc llvm-13-tools
 sudo apt-get install clang-14 clang-14-doc clang-tools-14 lld-14 llvm-14 \
 	llvm-14-doc llvm-14-tools
 sudo apt-get install clang-15 clang-15-doc clang-tools-15 lld-15 llvm-15 \
@@ -47,7 +54,11 @@ sudo apt-get install clang-16 clang-16-doc clang-tools-16 lld-16 llvm-16 \
 sudo apt-get install clang-17 clang-17-doc clang-tools-17 lld-17 llvm-17 \
 	llvm-17-doc llvm-17-tools
 sudo apt-get install clang-18 clang-18-doc clang-format-18 clang-tidy-18 \
-	clang-tools-18 clangd-18 lld-18 llvm-18 llvm-18-doc llvm-18-tools
+     clang-tools-18 lld-18 llvm-18 llvm-18-doc llvm-18-tools
+sudo apt-get install clang-19 clang-19-doc clang-format-19 clang-tidy-19 \
+     clang-tools-19 clangd-19 lld-19 llvm-19 llvm-19-doc llvm-19-tools
+# Absent in 24.10:
+sudo apt-get install libasan5 libstdc++-doc libgfortran-dev
 # DeepState (x86_64 only)
 sudo apt-get install libc6-dev-i386
 # Not found on AWS EC2
