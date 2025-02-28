057ffa862b9b64355294e3d7fa3bf1d647b75d20
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2025-02-28 15:36:08 +0200

    Emacs: update UI geometries for new laptop

--- emacs/.emacs.d/my/my-ui-geometry.el
+++ emacs/.emacs.d/my/my-ui-geometry.el
@@ -74,13 +74,21 @@
 (defconst dotfiles--m1star-laptop-frame-dimensions
   (make-dotfiles--frame-dimensions :top 1 :left 1 :height 67 :width 242))
 
-(defconst dotfiles--m1star-external-display-size '(3008 . 1692))
+(defconst dotfiles--m1star-external-display-size '(3360 . 1890))
 (defconst dotfiles--m1star-external-frame-dimensions
+  (make-dotfiles--frame-dimensions :top 0 :left 5568 :height 135 :width 477))
+
+(defconst dotfiles--work-laptop-display-size '(1728 . 1117))
+(defconst dotfiles--work-laptop-frame-dimensions
+  (make-dotfiles--frame-dimensions :top 1 :left 1 :height 67 :width 242))
+
+(defconst dotfiles--work-external-display-size '(3008 . 1692))
+(defconst dotfiles--work-external-frame-dimensions
   (make-dotfiles--frame-dimensions :top 0 :left 3008 :height 117 :width 427))
 
 (defconst dotfiles--ignored-display-sizes
   [(3600 . 1080) (5520 . 1080) (4688 . 1692) (3600 . 1692) (7744 . 1692)
-   (1920 . 1080)]
+   (1920 . 1080) (3840 . 2160) (8928 . 2160)]
   "Possible interim screen resolutions while docking/undocking to be ignored.")
 
 (defun dotfiles--warn-about-unknown-display-geometry (display-geometry)
@@ -96,6 +104,14 @@
          (dotfiles--add-frame-geometry-to-initial-alist
           dotfiles--darkstar-laptop-frame-dimensions)
          (two-windows))
+        ((equal display-geometry dotfiles--darkstar-external-display-size)
+         ;; darkstar with external screens: initial frame maximized in the
+         ;; middle screen
+         (dotfiles--add-frame-geometry-to-initial-alist
+          dotfiles--darkstar-external-frame-dimensions)
+         (add-to-list 'initial-frame-alist '(fullscreen . fullboth))
+         (add-to-list 'initial-frame-alist '(fullscreen-restore . maximized))
+         (eight-windows))
         ((equal display-geometry dotfiles--m1star-laptop-display-size)
          ;; m1star without external screens: initial frame positioned in the top
          ;; left corner
@@ -110,14 +126,21 @@
          (add-to-list 'initial-frame-alist '(fullscreen . fullboth))
          (add-to-list 'initial-frame-alist '(fullscreen-restore . maximized))
          (eight-windows))
-        ((equal display-geometry dotfiles--darkstar-external-display-size)
-         ;; darkstar with external screens: initial frame maximized in the
-         ;; middle screen
+        ((equal display-geometry dotfiles--work-laptop-display-size)
+         ;; work without external screens: initial frame positioned in the top
+         ;; left corner
          (dotfiles--add-frame-geometry-to-initial-alist
-          dotfiles--darkstar-external-frame-dimensions)
+          dotfiles--work-laptop-frame-dimensions)
+         (two-windows))
+        ((equal display-geometry dotfiles--work-external-display-size)
+         ;; work with external screens: initial frame maximized in the middle
+         ;; screen
+         (dotfiles--add-frame-geometry-to-initial-alist
+          dotfiles--work-external-frame-dimensions)
          (add-to-list 'initial-frame-alist '(fullscreen . fullboth))
          (add-to-list 'initial-frame-alist '(fullscreen-restore . maximized))
          (eight-windows))
+        ;; Ignored and unknown geometries
         ((seq-position dotfiles--ignored-display-sizes display-geometry)
          ())
         (t (dotfiles--warn-about-unknown-display-geometry display-geometry))))
@@ -132,7 +155,12 @@
     (message "Resizing from %s to %s" dotfiles--old-display-geometry
              new-display-geometry)
     (setq dotfiles--old-display-geometry new-display-geometry)
-    (cond ((equal new-display-geometry dotfiles--m1star-laptop-display-size)
+    (cond ((equal new-display-geometry dotfiles--work-laptop-display-size)
+           (dotfiles--move-to-frame-geometry
+            dotfiles--work-laptop-frame-dimensions)
+           (set-frame-parameter nil 'fullscreen 'maximized)
+           (two-windows))
+          ((equal new-display-geometry dotfiles--m1star-laptop-display-size)
            (dotfiles--move-to-frame-geometry
             dotfiles--m1star-laptop-frame-dimensions)
            (set-frame-parameter nil 'fullscreen 'maximized)
@@ -147,11 +175,17 @@
             dotfiles--darkstar-external-frame-dimensions)
            (set-frame-parameter nil 'fullscreen 'fullboth)
            (eight-windows))
+          ((equal new-display-geometry dotfiles--work-external-display-size)
+           (dotfiles--move-to-frame-geometry
+            dotfiles--work-external-frame-dimensions)
+           (set-frame-parameter nil 'fullscreen 'fullboth)
+           (eight-windows))
           ((equal new-display-geometry dotfiles--m1star-external-display-size)
            (dotfiles--move-to-frame-geometry
             dotfiles--m1star-external-frame-dimensions)
            (set-frame-parameter nil 'fullscreen 'fullboth)
            (eight-windows))
+          ;; Ignored and unknown geometries
           ((seq-position dotfiles--ignored-display-sizes
                          new-display-geometry) ())
           (t (dotfiles--warn-about-unknown-display-geometry
