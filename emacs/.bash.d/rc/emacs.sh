553f2994a927d4028e81f444f9f4988db608dc8c
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2025-04-14 13:12:23 +0300

    Shell: add emacs-nw alias

--- emacs/.bash.d/rc/emacs.sh
+++ emacs/.bash.d/rc/emacs.sh
@@ -2,3 +2,11 @@
 
 export EDITOR="emacsclient -c -r"
 export VISUAL="emacsclient -c -r"
+
+UNAME_OUT="$(uname -s)"
+
+if [ "$UNAME_OUT" = "Darwin" ]; then
+    alias emacs-nw="emacs -nw"
+else
+    alias emacs-nw="TERM=xterm-24bit emacs -nw"
+fi
