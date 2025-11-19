2c8db141de0915749029e9832829c9c700b59184
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2025-11-19 06:29:37 +0200

    Emacs package update

--- emacs/.emacs.d/elpa/transient-0.11.0/transient-pkg.el
+++ emacs/.emacs.d/elpa/transient-0.11.0/transient-pkg.el
@@ -1,12 +1,13 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "transient" "0.10.1"
+(define-package "transient" "0.11.0"
   "Transient commands."
-  '((emacs  "26.1")
-    (compat "30.1")
-    (seq    "2.24"))
+  '((emacs    "28.1")
+    (compat   "30.1")
+    (cond-let "0.2")
+    (seq      "2.24"))
   :url "https://github.com/magit/transient"
-  :commit "053d56e4de2dd78bf32f7af7ed5f289a91cdb6ac"
-  :revdesc "v0.10.1-0-g053d56e4de2d"
+  :commit "0d3f8d4fb6d41b841126820a06ecc98579bd8265"
+  :revdesc "v0.11.0-0-g0d3f8d4fb6d4"
   :keywords '("extensions")
   :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
   :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))
