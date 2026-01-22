32896b150632d2259e117d48c25b7b47f82f2ed0
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2026-01-22 06:37:45 +0200

    mysql-work.sh: add 9.6.0 support

--- mysql-work/.zsh.d/rc/mysql-work.sh
+++ mysql-work/.zsh.d/rc/mysql-work.sh
@@ -202,7 +202,7 @@ mysql_export_environment_helpers() {
     declare -gA cmake_flags=()
 
     declare -r min_version="8.0.18"
-    declare -r max_version="9.5.0"
+    declare -r max_version="9.6.0"
 
     # Platform-specific stuff, both building blocks and complete user variables
     if [ "$uname_out" = "Darwin" ]; then
@@ -402,6 +402,12 @@ mysql_export_environment_helpers() {
 
     mysql_add_cmake_flags "8.0.33" "${max_version}" any \
                           "-DFORCE_COLORED_OUTPUT=ON"
+
+    declare -a -r my960_comp_flags=(
+        "-DCMAKE_CXX_FLAGS=$(mysql_get_comp_flags 9.6.0 cxx)"
+        "-DCMAKE_CXX_FLAGS_DEBUG=$(mysql_get_comp_flags 9.6.0 cxx_debug)"
+        "-DCMAKE_CXX_FLAGS_RELEASE=$(mysql_get_comp_flags 9.6.0 cxx_release)"
+    )
 
     declare -a -r my950_comp_flags=(
         "-DCMAKE_CXX_FLAGS=$(mysql_get_comp_flags 9.5.0 cxx)"
@@ -692,6 +698,11 @@ mysql_export_environment_helpers() {
 
     # Paydirt!
 
+    export MY960D=("${myd[@]}" $(mysql_get_cmake_flags 9.6.0 any_debug)
+                   "${my960_comp_flags[@]}")
+    export MY960=("${myr[@]}" $(mysql_get_cmake_flags 9.6.0 any_release)
+                  "${my960_comp_flags[@]}")
+
     export MY950D=("${myd[@]}" $(mysql_get_cmake_flags 9.5.0 any_debug)
                    "${my950_comp_flags[@]}")
     export MY950=("${myr[@]}" $(mysql_get_cmake_flags 9.5.0 any_release)
@@ -969,6 +980,12 @@ mysql_cmake() {
         "mysql")
             echo "Configuring MySQL $major_ver.$minor_ver.$patch_level"
             case "$major_ver.$minor_ver.$patch_level" in
+                9.6.0)
+                    declare -a release_flags=("${MY960[@]}")
+                    declare -a debug_flags=("${MY960D[@]}")
+                    declare -a -r \
+                            core_dump_flags=("${MY8030_MAX_CORE_DUMP_FLAGS[@]}")
+                    ;;
                 9.5.0)
                     declare -a release_flags=("${MY950[@]}")
                     declare -a debug_flags=("${MY950D[@]}")
