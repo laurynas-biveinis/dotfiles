4014bd9bb2469e277b58f264255c5e04450ccdca
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2026-02-13 06:42:34 +0200

    setup-ubuntu.sh: minor tweaks

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -9,6 +9,7 @@ set -eu
 sudo apt update
 sudo apt dist-upgrade
 
+# If minimized:
 sudo apt install locales
 sudo locale-gen en_US.utf8
 sudo update-locale
@@ -32,31 +33,30 @@ sudo apt install manpages-dev binutils libc6-dev software-properties-common \
 	cmake-format clang-tidy neovim lynis shellcheck bat cpplint duf eza \
 	python3-pandas pipx
 # Compiler versions
-# Absent in 24.10
+# Absent in 24.10+
 sudo apt install g++-10 libgfortran-10-dev
-sudo apt install g++-11 libgfortran-11-dev
-sudo apt install g++-12 libgfortran-12-dev
-sudo apt install g++-13 libgfortran-13-dev
-sudo apt install g++-14 libgfortran-14-dev cpp-14-doc gcc-14-doc \
-	libstdc++-14-doc
+sudo apt install g++-11 libgfortran-11-dev g++-12 libgfortran-12-dev \
+     g++-13 libgfortran-13-dev g++-14 libgfortran-14-dev cpp-14-doc gcc-14-doc \
+     libstdc++-14-doc
 # Absent in 24.04+
 sudo apt install clang-12 clang-12-doc clang-tools-12 lld-12 llvm-12 \
 	llvm-12-doc llvm-12-tools
-# Absent in 24.10
+# Absent in 24.10+
 sudo apt install clang-13 clang-13-doc clang-tools-13 lld-13 llvm-13 \
-	llvm-13-doc llvm-13-tools
-sudo apt install clang-14 clang-14-doc clang-tools-14 lld-14 llvm-14 \
-	llvm-14-doc llvm-14-tools
+     llvm-13-doc llvm-13-tools
+# Absent in 25.10
 sudo apt install clang-15 clang-15-doc clang-tools-15 lld-15 llvm-15 \
-	llvm-15-doc llvm-15-tools
-sudo apt install clang-16 clang-16-doc clang-tools-16 lld-16 llvm-16 \
-	llvm-16-doc llvm-16-tools
-sudo apt install clang-17 clang-17-doc clang-tools-17 lld-17 llvm-17 \
-	llvm-17-doc llvm-17-tools
-sudo apt install clang-18 clang-18-doc clang-format-18 clang-tidy-18 \
-	clang-tools-18 lld-18 llvm-18 llvm-18-doc llvm-18-tools
-sudo apt install clang-19 clang-19-doc clang-format-19 clang-tidy-19 \
-	clang-tools-19 clangd-19 lld-19 llvm-19 llvm-19-doc llvm-19-tools
+     llvm-15-doc llvm-15-tools \
+     clang-16 clang-16-doc clang-tools-16 lld-16 llvm-16 \
+     llvm-16-doc llvm-16-tools
+sudo apt install clang-14 clang-14-doc clang-tools-14 lld-14 llvm-14 \
+     llvm-14-doc llvm-14-tools \
+     clang-17 clang-17-doc clang-tools-17 lld-17 llvm-17 \
+     llvm-17-doc llvm-17-tools \
+     clang-18 clang-18-doc clang-format-18 clang-tidy-18 \
+     clang-tools-18 lld-18 llvm-18 llvm-18-doc llvm-18-tools \
+     clang-19 clang-19-doc clang-format-19 clang-tidy-19 \
+     clang-tools-19 clangd-19 lld-19 llvm-19 llvm-19-doc llvm-19-tools
 # Absent in 24.10:
 sudo apt install libasan5 libstdc++-doc libgfortran-dev
 # DeepState (x86_64 only)
