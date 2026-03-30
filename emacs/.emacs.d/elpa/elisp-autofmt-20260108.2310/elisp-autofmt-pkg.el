c2ecb11e4d93532de6e6f4ddd26510de4f18a1df
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2026-03-30 11:51:05 +0300

    Emacs package update

--- emacs/.emacs.d/elpa/elisp-autofmt-20260108.2310/elisp-autofmt-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "elisp-autofmt" "20260108.2310"
-  "Emacs lisp auto-format."
-  '((emacs "29.1"))
-  :url "https://codeberg.org/ideasman42/emacs-elisp-autofmt"
-  :commit "e6c0aac8ebd8b7a7213fbf18934878735bf7ae26"
-  :revdesc "e6c0aac8ebd8"
-  :authors '(("Campbell Barton" . "ideasman42@gmail.com"))
-  :maintainers '(("Campbell Barton" . "ideasman42@gmail.com")))
---
+++ emacs/.emacs.d/elpa/elisp-autofmt-20260329.1053/elisp-autofmt-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "elisp-autofmt" "20260329.1053"
+  "Emacs lisp auto-format."
+  '((emacs "29.1"))
+  :url "https://codeberg.org/ideasman42/emacs-elisp-autofmt"
+  :commit "c28b2fb8b45106b355b3df1f4751ae32d1a9350f"
+  :revdesc "c28b2fb8b451"
+  :authors '(("Campbell Barton" . "ideasman42@gmail.com"))
+  :maintainers '(("Campbell Barton" . "ideasman42@gmail.com")))
