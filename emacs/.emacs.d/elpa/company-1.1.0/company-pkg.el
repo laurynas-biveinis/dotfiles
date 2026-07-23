9fa4ba7e9f36120e89f0102e005b849706af2359
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2026-07-23 08:04:04 +0300

    Emacs package update

---
+++ emacs/.emacs.d/elpa/company-1.1.0/company-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "company" "1.1.0"
+  "Modular text completion framework."
+  '((emacs    "26.1")
+    (posframe "1.5.1"))
+  :url "http://company-mode.github.io/"
+  :commit "1cc907ac9e46ae4209eb5a341131787e0c678406"
+  :revdesc "1.1.0-0-g1cc907ac9e46"
+  :keywords '("abbrev" "convenience" "matching")
+  :maintainers '(("Dmitry Gutov" . "dmitry@gutov.dev")))
