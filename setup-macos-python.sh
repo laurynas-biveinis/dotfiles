6a4f3d94d4451308c04c4ab69d706cb94ddacaf7
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2026-09-10 21:32:24 +0300

    Python: Refresh asdf shims after installing development tools

--- setup-macos-python.sh
+++ setup-macos-python.sh
@@ -33,3 +33,4 @@ PIP_REQUIRE_VIRTUALENV="0" pip install "python-lsp-server[all]" pylsp-mypy \
 	python-lsp-isort pyls-memestra jedi pylsp-rope python-lsp-ruff \
 	python-lsp-black pylint pycodestyle pyflakes pandas openpyxl \
 	pandas-stubs scipy
+asdf reshim python
